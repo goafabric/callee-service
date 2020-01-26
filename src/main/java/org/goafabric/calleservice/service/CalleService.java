a1ec1e97ee772a2e48abacac8a8bfbc905ddd6f6
goafabric <amautsch@gmx.de>
2020-01-26 10:19:59 +0100

    website added

--- src/main/java/org/goafabric/calleservice/service/CalleService.java
+++ src/main/java/org/goafabric/calleservice/service/CalleService.java
@@ -3,4 +3,5 @@ package org.goafabric.calleservice.service;
 public interface CalleService {
     String RESOURCE = "callees";
     Boolean isAlive();
+    String sayMyName(String name);
 }
--- src/main/java/org/goafabric/calleservice/service/CalleServiceBean.java
+++ src/main/java/org/goafabric/calleservice/service/CalleServiceBean.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +19,11 @@ public class CalleServiceBean implements CalleService {
     @GetMapping("isAlive")
     public Boolean isAlive() {
         return calleeService.isAlive();
+    }
+
+    @Override
+    @GetMapping("sayMyName")
+    public String sayMyName(@RequestParam String name) {
+        return "Your name is: " + name;
     }
 }
