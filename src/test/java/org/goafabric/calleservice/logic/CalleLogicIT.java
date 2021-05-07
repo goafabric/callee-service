8009e99741fd738214339309cb71e1a0615f78bd
Andreas Mautsch <amautsch>
2021-05-07 14:07:15 +0200

    junit5

---
+++ src/test/java/org/goafabric/calleservice/logic/CalleLogicIT.java
@@ -0,0 +1,31 @@
+package org.goafabric.calleservice.logic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CalleLogicIT {
+    @Autowired
+    private CalleLogic calleLogic;
+
+    @Test
+    void isAlive() {
+        assertThat(calleLogic.isAlive()).isEqualTo(Boolean.TRUE);
+    }
+
+    @Test
+    void sayMyName() {
+        assertThat(calleLogic.sayMyName("SlimShady")).isEqualTo("Your name is: SlimShady");
+    }
+
+    @Test
+    void sayMyOtherName() {
+        assertThat(calleLogic.sayMyOtherName("SlimShady")).isEqualTo("Your name is: SlimShady");
+    }
+}
