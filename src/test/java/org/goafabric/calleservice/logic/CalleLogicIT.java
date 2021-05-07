6ee0e37c1bce4f9089499bcfac246a432c28c02e
Andreas Mautsch <amautsch>
2021-05-07 14:12:39 +0200

    junit5

--- src/test/java/org/goafabric/calleservice/logic/CalleLogicIT.java
+++ src/test/java/org/goafabric/calleservice/logic/CalleLogicIT.java
@@ -1,14 +1,12 @@
 package org.goafabric.calleservice.logic;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@ExtendWith(SpringExtension.class)
+//@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CalleLogicIT {
     @Autowired
