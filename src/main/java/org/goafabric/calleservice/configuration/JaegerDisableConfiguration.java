6c5f5ddf0d86d715c80a71ab4699229dab204f59
Andreas Mautsch <amautsch>
2021-05-09 10:59:40 +0200

    junit5

--- src/main/java/org/goafabric/calleservice/configuration/JaegerDisableConfiguration.java
+++ src/main/java/org/goafabric/calleservice/configuration/JaegerDisableConfiguration.java
@@ -1,16 +1,19 @@
 package org.goafabric.calleservice.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-@ConditionalOnProperty(value = "opentracing.jaeger.enabled", havingValue = "false", matchIfMissing = false)
+@ConditionalOnProperty(value = "opentracing.jaeger.disabled", havingValue = "true", matchIfMissing = true)
 @Configuration
+@Slf4j
 public class JaegerDisableConfiguration {
 
     @Bean
     public io.opentracing.Tracer jaegerTracer() {
+        log.info("Jaeger disabled");
         return io.opentracing.noop.NoopTracerFactory.create();
     }
 }
