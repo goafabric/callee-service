e526fa1ffa735b04101aab004688c1cb044ac549
Andreas Mautsch <amautsch>
2021-05-13 15:40:07 +0200

    build pack update

--- build-native.sh
+++ build-native.sh
@@ -2,7 +2,7 @@
 git pull
 time mvn clean spring-boot:build-image -P docker-image-native
 #time mvn clean spring-boot:build-image install -P docker-image-native
-docker run --name spring-boot-example-graal-native --rm -p50900:50900 goafabric/calle-service-native:1.0.2-SNAPSHOT
+docker run --name callee-service-native --rm -p50900:50900 goafabric/calle-service-native:1.0.2-SNAPSHOT
 
-#docker pull goafabric/calle-service-native:1.0.2-SNAPSHOT && docker run --name spring-boot-example-graal-native --rm -p50900:50900 goafabric/calle-service-native:1.0.2-SNAPSHOT -Xmx64m
+#docker pull goafabric/calle-service-native:1.0.2-SNAPSHOT && docker run --name callee-service-native --rm -p50900:50900 goafabric/calle-service-native:1.0.2-SNAPSHOT -Xmx64m
 
