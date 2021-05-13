bb3cf4dce1cd12fd048cd2d7e045f4c394549709
Andreas Mautsch <amautsch>
2021-05-13 11:49:23 +0200

    upgrade to 1.0.2 snpapshot

--- build-native.sh
+++ build-native.sh
@@ -2,6 +2,6 @@
 git pull
 time mvn clean spring-boot:build-image -P docker-image-native
 #time mvn clean spring-boot:build-image install -P docker-image-native
-docker run --name spring-boot-example-graal-native --rm -p50900:50900 goafabric/calle-service-native:1.0.1-SNAPSHOT
+docker run --name spring-boot-example-graal-native --rm -p50900:50900 goafabric/calle-service-native:1.0.2-SNAPSHOT
 
-#docker pull goafabric/calle-service-native:1.0.1-SNAPSHOT && docker run --name spring-boot-example-graal-native --rm -p50900:50900 goafabric/calle-service-native:1.0.1-SNAPSHOT
+#docker pull goafabric/calle-service-native:1.0.2-SNAPSHOT && docker run --name spring-boot-example-graal-native --rm -p50900:50900 goafabric/calle-service-native:1.0.2-SNAPSHOT
