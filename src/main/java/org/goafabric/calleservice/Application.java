d7ac4f969f1f38a08dc264b46d361fc0d4507fab
Andreas Mautsch <amautsch>
2021-05-22 19:45:25 +0200

    integrity check added

--- src/main/java/org/goafabric/calleservice/Application.java
+++ src/main/java/org/goafabric/calleservice/Application.java
@@ -1,10 +1,13 @@
 package org.goafabric.calleservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by amautsch on 26.06.2015.
@@ -16,5 +19,14 @@ public class Application {
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner init(ApplicationContext context) {
+        return args -> {
+            if ((args.length > 0) && ("-check-integrity".equals(args[0]))) {
+                SpringApplication.exit(context, () -> 0);
+            }
+        };
     }
 }
