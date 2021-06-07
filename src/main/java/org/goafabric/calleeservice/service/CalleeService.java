52f2306df41da4e140fa4ffe31c5d5fc30e86440
Andreas Mautsch <amautsch>
2021-06-07 08:17:13 +0200

    refactoring

--- src/main/java/org/goafabric/calleeservice/service/CalleeService.java
+++ src/main/java/org/goafabric/calleeservice/service/CalleeService.java
@@ -10,8 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "callees",
-        produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "callees", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CalleeService {
     @Autowired
     CalleeLogic calleeLogic;
