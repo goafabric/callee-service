76d5101ce684436e72ad8c2afe65af0e7684cdb3
Andreas Mautsch <amautsch>
2021-06-30 15:37:08 +0200

    callee changes

---
+++ src/main/java/org/goafabric/calleeservice/logic/Callee.java
@@ -0,0 +1,14 @@
+package org.goafabric.calleeservice.logic;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Callee {
+    private String message;
+}
--- src/main/java/org/goafabric/calleeservice/logic/CalleeLogic.java
+++ src/main/java/org/goafabric/calleeservice/logic/CalleeLogic.java
@@ -7,22 +7,26 @@ import org.springframework.stereotype.Component;
 public class CalleeLogic {
     private Long sleepTime = 0l;
 
+    public Callee sayMyName(String name) {
+        sleep();
+        return Callee.builder()
+                .message("Your name is: " + name).build();
+    }
+
+    public Callee sayMyOtherName(String name) {
+        sleep();
+        return Callee.builder()
+                .message("Your name is: " + name).build();
+    }
+
+    public Callee setSleepTime(Long sleepTime) {
+        this.sleepTime = sleepTime; //you should never change instance variables for a productive app
+        return Callee.builder()
+                .message("set sleepTime to: " + sleepTime).build();
+    }
+
     @SneakyThrows
-    public Boolean isAlive() {
+    private void sleep() {
         Thread.sleep(sleepTime);
-        return Boolean.TRUE;
-    }
-
-    public String setSleepTime(Long sleepTime) {
-        this.sleepTime = sleepTime;
-        return "set sleepTime to: " + sleepTime;
-    }
-
-    public String sayMyName(String name) {
-        return "Your name is: " + name;
-    }
-
-    public String sayMyOtherName(String name) {
-        return "Your name is: " + name;
     }
 }
