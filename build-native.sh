65358b8d0ade4dfbf70d4f5a90944f8534a2d81c
Andreas Mautsch <amautsch>
2021-08-09 15:42:42 +0200

    release 1.1.0

--- build-native.sh
+++ build-native.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
 time mvn clean -P docker-image-native
-docker run --name callee-service-native --rm -p50900:50900 goafabric/callee-service-native:1.1.0-SNAPSHOT
+docker run --name callee-service-native --rm -p50900:50900 goafabric/callee-service-native:1.1.0
