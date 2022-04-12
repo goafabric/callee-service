abbff86cd732de23e5e255cf304460f16d8d6e8c
andreas <andreas>
2022-04-12 12:51:46 +0200

    HttpInterceptor added

--- src/main/java/org/goafabric/calleeservice/Application.java
+++ src/main/java/org/goafabric/calleeservice/Application.java
@@ -34,7 +34,7 @@ public class Application {
 
         @Override
         protected void configure(final HttpSecurity httpSecurity) throws Exception {
-            if (isAuthenticationEnabled) { super.configure(httpSecurity); }
+            if (isAuthenticationEnabled) { httpSecurity.authorizeRequests().anyRequest().authenticated().and().httpBasic(); }
             else { httpSecurity.authorizeRequests().anyRequest().permitAll(); }
         }
     }
