ae8e5c58858fb4c3ef63ad04416b5bfaa1d81416
andreas <andreas>
2022-04-12 13:56:13 +0200

    HttpInterceptor added

--- src/main/java/org/goafabric/calleeservice/crossfunctional/DurationLogger.java
+++ src/main/java/org/goafabric/calleeservice/crossfunctional/DurationLogger.java
@@ -29,7 +29,7 @@ public class DurationLogger {
             return joinPoint.proceed();
         } finally {
             final Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
-            log.info("{} took {}ms for user {}", toString(method), System.currentTimeMillis() - startTime, HttpInterceptor.getUserName());
+            log.info("{} took {}ms for user: {} , tenant: {}", toString(method), System.currentTimeMillis() - startTime, HttpInterceptor.getUserName(), HttpInterceptor.getTenantId());
         }
     }
 
