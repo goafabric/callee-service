826d6cd9a0fc3fe9a1199174f59089e4e5724891
Andreas Mautsch <amautsch>
2022-05-14 07:54:47 +0200

    merge from refactoring

--- src/main/java/org/goafabric/calleeservice/crossfunctional/HttpInterceptor.java
+++ src/main/java/org/goafabric/calleeservice/crossfunctional/HttpInterceptor.java
@@ -1,5 +1,7 @@
 package org.goafabric.calleeservice.crossfunctional;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,8 +9,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @Slf4j
