5d6310465a623184634ba369325425d13f99a6d1
Andreas Mautsch <amautsch>
2022-07-24 08:58:46 +0200

    authentication moved

--- src/main/java/org/goafabric/calleeservice/crossfunctional/HttpInterceptor.java
+++ src/main/java/org/goafabric/calleeservice/crossfunctional/HttpInterceptor.java
@@ -1,8 +1,12 @@
 package org.goafabric.calleeservice.crossfunctional;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -36,5 +40,15 @@ public class HttpInterceptor implements WebMvcConfigurer {
                 userName.remove();
             }
         });
+    }
+
+    @Value("${security.authentication.enabled:true}")
+    private Boolean isAuthenticationEnabled;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        if (isAuthenticationEnabled) { http.authorizeRequests().anyRequest().authenticated().and().httpBasic().and().csrf().disable(); }
+        else { http.authorizeRequests().anyRequest().permitAll(); }
+        return http.build();
     }
 }
