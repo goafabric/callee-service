9b9fffd9f2bd9c6d2523e4b403b39db349b38edd
Andreas Mautsch <amautsch>
2022-09-26 09:13:22 +0200

    jaeger changes

--- src/main/java/org/goafabric/calleeservice/Application.java
+++ src/main/java/org/goafabric/calleeservice/Application.java
@@ -1,10 +1,15 @@
 package org.goafabric.calleeservice;
 
+import io.micrometer.observation.ObservationRegistry;
+import jakarta.servlet.DispatcherType;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.observation.HttpRequestsObservationFilter;
 
 
 /**
@@ -22,5 +27,14 @@ public class Application {
     public CommandLineRunner init(ApplicationContext context) {
         return args -> {if ((args.length > 0) && ("-check-integrity".equals(args[0]))) {SpringApplication.exit(context, () -> 0);}};
     }
-    
+
+    @Bean
+    FilterRegistrationBean traceWebFilter(ObservationRegistry observationRegistry) {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new HttpRequestsObservationFilter(observationRegistry));
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.ASYNC, DispatcherType.ERROR, DispatcherType.FORWARD,
+                DispatcherType.INCLUDE, DispatcherType.REQUEST);
+        filterRegistrationBean.setOrder(Ordered.LOWEST_PRECEDENCE);
+        return filterRegistrationBean;
+    }
+
 }
