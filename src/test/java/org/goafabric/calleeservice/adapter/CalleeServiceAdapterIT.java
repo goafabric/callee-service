7111733f49c795bb747653f8137ffc49b6da2729
Andreas Mautsch <amautsch>
2022-10-23 09:44:01 +0200

    refactoring

--- src/test/java/org/goafabric/calleeservice/adapter/CalleeServiceAdapterIT.java
+++ src/test/java/org/goafabric/calleeservice/adapter/CalleeServiceAdapterIT.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class CalleeServiceAdapterNRIT {
+public class CalleeServiceAdapterIT {
     @Autowired
     CalleeServiceAdapter calleeServiceAdapter;
 
