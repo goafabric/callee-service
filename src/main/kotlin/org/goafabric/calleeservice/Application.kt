54fbe1817eb13f48a2084d70aca8e0f7b139e6ec
Andreas Mautsch <amautsch>
2022-10-28 12:10:16 +0200

    folders renamed to kotlin

--- src/main/kotlin/org/goafabric/calleeservice/Application.kt
+++ src/main/kotlin/org/goafabric/calleeservice/Application.kt
@@ -1,32 +1,11 @@
-/*
 package org.goafabric.calleeservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.CommandLineRunner
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.ExitCodeGenerator
-import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.Bean
-import kotlin.jvm.JvmStatic
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Application {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
-        }
-    }
+class DemoApplication
 
-    @Bean
-    fun init(context: ApplicationContext?): CommandLineRunner {
-        return CommandLineRunner { args: Array<String> ->
-            if (args.size > 0 && "-check-integrity" == args[0]) {
-                SpringApplication.exit(context, ExitCodeGenerator { 0 })
-            }
-        }
-    }
-
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
 }
-
- */
---
+++ src/main/kotlin/org/goafabric/calleeservice/ApplicationRunner.kt
@@ -0,0 +1,22 @@
+package org.goafabric.calleeservice
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.ExitCodeGenerator
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class ApplicationRunner {
+    @Bean
+    fun runner(context: ApplicationContext?): CommandLineRunner? {
+        return CommandLineRunner { args: Array<String> ->
+            if (args.isNotEmpty() && "-check-integrity" == args[0]) {
+                SpringApplication.exit(context, ExitCodeGenerator { 0 })
+            }
+        }
+    }
+
+}
