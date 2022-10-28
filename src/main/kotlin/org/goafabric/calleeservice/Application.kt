d4f217816cdf83a13e17cbe42457c48aac503680
Andreas Mautsch <amautsch>
2022-10-28 12:21:03 +0200

    native fix

--- src/main/kotlin/org/goafabric/calleeservice/Application.kt
+++ src/main/kotlin/org/goafabric/calleeservice/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+    runApplication<Application>(*args)
 }
