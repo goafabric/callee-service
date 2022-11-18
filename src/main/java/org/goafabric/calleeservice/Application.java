1c9c1188ce9339ba77265a4a3f110b11c145edba
Andreas Mautsch <amautsch>
2022-11-18 10:55:13 +0100

    changes

--- src/main/java/org/goafabric/calleeservice/Application.java
+++ src/main/java/org/goafabric/calleeservice/Application.java
@@ -3,8 +3,11 @@ package org.goafabric.calleeservice;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.core.io.ClassPathResource;
 
@@ -30,8 +33,11 @@ public class Application {
         try { Thread.currentThread().join(5000);} catch (InterruptedException e) {}
     }
 
-
-
+    @Bean
+    public CommandLineRunner init(ApplicationContext context) {
+        return args -> {if ((args.length > 0) && ("-check-integrity".equals(args[0]))) {SpringApplication.exit(context, () -> 0);}};
+    }
+    
     private static void doReflection() {
         final Class clazz;
         try {
