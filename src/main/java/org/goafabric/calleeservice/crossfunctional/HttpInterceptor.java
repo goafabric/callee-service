22968f84a8c6a1576d8c09304c5ed837e1357188
Andreas Mautsch <amautsch>
2022-11-30 08:31:17 +0100

    party disable spring security traceing

--- src/main/java/org/goafabric/calleeservice/crossfunctional/HttpInterceptor.java
+++ src/main/java/org/goafabric/calleeservice/crossfunctional/HttpInterceptor.java
@@ -4,11 +4,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -58,5 +61,16 @@ public class HttpInterceptor implements WebMvcConfigurer {
         return (name, context) -> !name.startsWith("spring.security.");
     }
     */
+
+    @Component
+    class FilterChainProxyPostProcessor implements BeanPostProcessor {
+        @Override
+        public Object postProcessBeforeInitialization(Object bean, String beanName) {
+            if (bean instanceof FilterChainProxy) {
+                ((FilterChainProxy) bean).setFilterChainDecorator(new FilterChainProxy.VirtualFilterChainDecorator());
+            }
+            return bean;
+        }
+    }
 
 }
