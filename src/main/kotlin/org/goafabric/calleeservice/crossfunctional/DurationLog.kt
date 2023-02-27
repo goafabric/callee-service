203132212aeb0388aa081f3f1d8be3d72b3fba3b
Andreas Mautsch <amautsch>
2023-02-27 10:52:11 +0100

    http interceptor updated

---
+++ src/main/kotlin/org/goafabric/calleeservice/crossfunctional/DurationLog.kt
@@ -0,0 +1,10 @@
+package org.goafabric.calleeservice.crossfunctional
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.CLASS
+)
+annotation class DurationLog 
