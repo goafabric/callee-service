495e0b48d7ccdb4a20195fa85c03d931577cb2c2
Andreas Mautsch <amautsch>
2023-02-27 10:44:44 +0100

    http interceptor updated

--- src/main/kotlin/org/goafabric/calleeservice/crossfunctional/HttpInterceptor.kt
+++ src/main/kotlin/org/goafabric/calleeservice/crossfunctional/HttpInterceptor.kt
@@ -1,10 +1,16 @@
 package org.goafabric.calleeservice.crossfunctional
 
+import io.micrometer.observation.Observation
+import io.micrometer.observation.ObservationPredicate
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import lombok.extern.slf4j.Slf4j
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -12,6 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 @Slf4j
 class HttpInterceptor : WebMvcConfigurer {
+
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(object : HandlerInterceptor {
             override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
@@ -20,28 +27,40 @@ class HttpInterceptor : WebMvcConfigurer {
                 return true
             }
 
-            override fun afterCompletion(
-                request: HttpServletRequest,
-                response: HttpServletResponse,
-                handler: Any,
-                ex: Exception?
-            ) {
+            override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
                 tenantId.remove()
                 userName.remove()
             }
         })
     }
 
-
     companion object {
-        private val tenantId = ThreadLocal<String?>()
-        private val userName = ThreadLocal<String?>()
-        fun getTenantId(): String? {
+        private val tenantId = ThreadLocal<String>()
+        private val userName = ThreadLocal<String>()
+        fun getTenantId(): String {
             return if (tenantId.get() != null) tenantId.get() else "0" //tdo
         }
 
-        fun getUserName(): String? {
+        fun getUserName(): String {
             return if (userName.get() != null) userName.get() else if (SecurityContextHolder.getContext().authentication != null) SecurityContextHolder.getContext().authentication.name else ""
         }
     }
+
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity, @Value("\${security.authentication.enabled:true}") isAuthenticationEnabled: Boolean): SecurityFilterChain {
+        return if (isAuthenticationEnabled) http.authorizeHttpRequests().anyRequest().authenticated().and().httpBasic()
+            .and().csrf().disable().build() else http.authorizeHttpRequests().anyRequest().permitAll().and().build()
+    }
+
+    /*
+    @Bean
+    fun disableHttpServerObservationsFromName(): ObservationPredicate {
+        return ObservationPredicate { name: String, context: Observation.Context? -> !name.startsWith("spring.security.") }
+    }
+
+     */
+
+
 }
