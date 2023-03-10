5e2f0dd846e8a64e74c32fbaeb876dbcafd4b226
Andreas Mautsch <amautsch>
2023-03-10 14:16:32 +0100

    nonnull removed

--- src/main/java/org/goafabric/calleeservice/logic/CalleeLogic.java
+++ src/main/java/org/goafabric/calleeservice/logic/CalleeLogic.java
@@ -1,6 +1,5 @@
 package org.goafabric.calleeservice.logic;
 
-import lombok.NonNull;
 import org.goafabric.calleeservice.controller.Callee;
 import org.goafabric.calleeservice.crossfunctional.DurationLog;
 import org.springframework.stereotype.Component;
@@ -8,17 +7,17 @@ import org.springframework.stereotype.Component;
 @Component
 @DurationLog
 public class CalleeLogic {
-    public Callee sayMyName(@NonNull String name) {
+    public Callee sayMyName(String name) {
         return Callee.builder()
                 .message("Your name is: " + name).build();
     }
 
-    public Callee sayMyOtherName(@NonNull String name) {
+    public Callee sayMyOtherName(String name) {
         return Callee.builder()
                 .message("Your name is: " + name).build();
     }
 
-    public Callee save(@NonNull Callee callee) {
+    public Callee save(Callee callee) {
         return Callee.builder()
                 .message("Storing your message: " + callee.getMessage()).build();
     }
