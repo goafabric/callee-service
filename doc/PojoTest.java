5c1e686578b849c96f61ad9e02469367bfeec6f0
Andreas Mautsch <amautsch>
2023-03-11 10:52:09 +0100

    Pojotest added

---
+++ doc/PojoTest.java
@@ -0,0 +1,61 @@
+package org.goafabric.spring.boot.examplebatch;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+public class PojoTest {
+
+    // Java 11 Lombok
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    class PersonL {
+        private String id;
+        private String lastName;
+        private String firstName;
+    }
+
+    @Test
+    void lombok() {
+        final PersonL person = PersonL.builder()
+                .firstName("firstName")
+                .lastName("lastName")
+                .build();
+    }
+
+    // Java 17 Records
+    record PersonR (
+            String id,
+            String firstName,
+            String lastName
+    ) {}
+    
+    @Test
+    void java17() {
+        final var person = new PersonR(
+                null,
+                "firstname",
+                "lastname"
+        );
+    }
+
+    // Kotlin
+    data class PersonK (
+            val id: String? = null,
+            val firstName: String,
+            val lastName: String
+    )
+
+    @Test
+    fun kotlin() {
+        val person = PersonK(
+                firstName = "firstName",
+                lastName = "lastName"
+        )
+    }
+
+}
