f43c9f7741dec424b37623e7225c92563965ef3b
Andreas Mautsch <amautsch>
2023-03-15 11:20:26 +0100

    upgrade

--- src/main/kotlin/org/goafabric/calleeservice/logic/CalleeLogic.kt
+++ src/main/kotlin/org/goafabric/calleeservice/logic/CalleeLogic.kt
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Component
 @Component
 class CalleeLogic {
     fun sayMyName (name : String) : Callee {
-        return Callee(message = "Your name is: $name")
+        return Callee(id = "0", message = "Your name is: $name")
     }
 
     fun sayMyOtherName (name : String) : Callee {
-        return Callee(message = "Your name is: $name")
+        return Callee(id = "0", message = "Your name is: $name")
     }
 
 
     fun save(callee: Callee): Callee? {
-        return Callee(message = "Storing your message: $callee.message")
+        return Callee(id = "0", message = "Storing your message: $callee.message")
     }
 }
