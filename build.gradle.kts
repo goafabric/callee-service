25b7f3ab8193148ef27ce66fd2324a5511c3ba0c
Andreas Mautsch <amautsch>
2023-04-13 14:47:42 +0200

    version increase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 group = "org.goafabric"
-version = "3.0.5-kts-SNAPSHOT"
+version = "3.0.5-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 plugins {
