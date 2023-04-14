621d9142e31f4acbe98bfb748b8d773591823f37
Andreas Mautsch <amautsch>
2023-04-14 11:03:40 +0200

    kt file added

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
+group = "org.goafabric"
+version = "3.0.5-console-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+plugins {
+	java
+	jacoco
+	id("org.springframework.boot") version "3.0.5"
+	id("io.spring.dependency-management") version "1.1.0"
+	id("org.graalvm.buildtools.native") version "0.9.20"
+	id("com.google.cloud.tools.jib") version "3.3.1"
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	constraints {
+		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.4")
+		implementation("org.mapstruct:mapstruct:1.5.3.Final")
+		annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
+		implementation("io.github.resilience4j:resilience4j-spring-boot3:2.0.2")
+	}
+}
+
+dependencies {
+	//web
+	implementation("org.springframework.boot:spring-boot-starter")
+
+	//crosscuting
+	implementation("org.springframework.boot:spring-boot-starter-aop")
+
+	implementation("org.springframework.boot:spring-boot-starter-cache")
+	implementation("com.github.ben-manes.caffeine:caffeine")
+
+	//test
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+	exclude("**/*NRIT*")
+	finalizedBy("jacocoTestReport")
+}
+
+val dockerRegistry = "goafabric"
+val nativeBuilderImage = "dashaun/builder:20230225"
+val baseImage = "ibm-semeru-runtimes:open-17.0.6_10-jre-focal@sha256:739eab970ff538cf22a20b768d7755dad80922a89b73b2fddd80dd79f9b880a1"
+val archSuffix = if (System.getProperty("os.arch").equals("aarch64")) "-arm64v8" else ""
+
+jib {
+	val amd64 = com.google.cloud.tools.jib.gradle.PlatformParameters(); amd64.os = "linux"; amd64.architecture = "amd64"
+	val arm64 = com.google.cloud.tools.jib.gradle.PlatformParameters(); arm64.os = "linux"; arm64.architecture = "arm64"
+	from.image = baseImage
+	to.image = "${dockerRegistry}/${project.name}:${project.version}"
+	container.jvmFlags = listOf("-Xms256m", "-Xmx256m")
+	from.platforms.set(listOf(amd64, arm64))
+}
+
+tasks.named<BootBuildImage>("bootBuildImage") {
+	builder.set(nativeBuilderImage)
+	imageName.set("${dockerRegistry}/${project.name}-native${archSuffix}:${project.version}")
+	environment.set(mapOf("BP_NATIVE_IMAGE" to "true", "BP_JVM_VERSION" to "17", "BP_NATIVE_IMAGE_BUILD_ARGUMENTS" to "-J-Xmx4000m"))
+}
+
+task<Exec>("dockerImageNativeRun") { dependsOn("bootBuildImage")
+	commandLine ("docker", "run", "--rm", "${dockerRegistry}/${project.name}-native${archSuffix}:${project.version}", "-check-integrity")
+}
+
+task<Exec>("dockerImageNative") { dependsOn("dockerImageNativeRun")
+	commandLine("docker", "push", "${dockerRegistry}/${project.name}-native${archSuffix}:${project.version}")
+}
