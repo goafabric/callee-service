b8ceee407c5ab58d79810d84444c2fa1a8c1c25c
Andreas Mautsch <amautsch>
2023-04-18 14:23:02 +0200

    vault added

--- doc/examples/vault/VaultConfiguration.java
+++ doc/examples/vault/VaultConfiguration.java
@@ -1,5 +1,8 @@
 package org.goafabric.calleeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,30 +11,39 @@ import org.springframework.vault.core.VaultTemplate;
 
 @Configuration
 public class VaultConfiguration {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private VaultTemplate vaultTemplate;
+
     record Credentials(String userName, String password) {}
 
     @Bean
-    public CommandLineRunner vault(VaultTemplate vaultTemplate) {
+    public CommandLineRunner vault() {
         return args -> {
-            //if ((args.length > 0) && ("-create-creds".equals(args[0]))) {
-                var credentials = new Credentials("homer simpson", "super-secret");
-                var vaultKeyValueOperations = vaultTemplate.opsForKeyValue("secret",
-                        VaultKeyValueOperationsSupport.KeyValueBackend.KV_2);
-
-                //write
-                vaultKeyValueOperations.put(credentials.userName, credentials);
-
-                //read
-                var response = vaultKeyValueOperations.get(credentials.userName, Credentials.class);
-
-                assert response != null;
-                System.out.println(response.getData().userName);
-                System.out.println(response.getData().password());
-            //}
+            //if (!((args.length > 0) && ("-create-creds".equals(args[0])))) { return; }
+            createCredentials();
         };
     }
 
-//implementation("org.springframework.cloud:spring-cloud-starter-vault-config:4.0.0")
+    private void createCredentials() {
+        var credentials = new Credentials("homer simpson", "super-secret");
+        var vaultKeyValueOperations = vaultTemplate.opsForKeyValue("secret",
+                VaultKeyValueOperationsSupport.KeyValueBackend.KV_2);
+
+        //write
+        vaultKeyValueOperations.put(credentials.userName, credentials);
+
+        //read
+        var response = vaultKeyValueOperations.get(credentials.userName, Credentials.class);
+
+        assert response != null;
+        log.info(response.getData().userName);
+        log.info(response.getData().password());
+    }
+
+    //implementation("org.springframework.cloud:spring-cloud-starter-vault-config:4.0.0")
+    //docker run --name vault --rm --cap-add=IPC_LOCK  -p8200:8200 -e 'VAULT_DEV_ROOT_TOKEN_ID=myroot' vault:1.13.1
 }
 
 
