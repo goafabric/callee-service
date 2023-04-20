3e2ff1933bcd4346059c4361ecc1cc3c60001205
Andreas Mautsch <amautsch>
2023-04-20 15:01:30 +0200

    spring boot upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,8 +50,11 @@ dependencies {
 	//crosscuting
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-aop")
+
+	//cloud
 	//implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	//implementation("org.springframework.cloud:spring-cloud-starter-vault-config:4.0.0")
+	//implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.0.1")
 
 	//test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
