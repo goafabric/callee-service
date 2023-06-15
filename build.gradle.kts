999062fc505986ef18a0f9e6ac2254f4664b1aed
Andreas Mautsch <amautsch>
2023-06-15 09:36:30 +0200

    otlp

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,8 +43,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("io.micrometer:micrometer-registry-prometheus")
 
-	implementation("io.micrometer:micrometer-tracing-bridge-brave") //implementation("io.micrometer:micrometer-tracing-bridge-otel")
-	implementation("io.zipkin.reporter2:zipkin-reporter-brave") //implementation("io.opentelemetry:opentelemetry-exporter-zipkin") //-otlp
+	implementation("io.micrometer:micrometer-tracing-bridge-brave")
+	implementation("io.zipkin.reporter2:zipkin-reporter-brave")
+
+	//implementation("io.micrometer:micrometer-tracing-bridge-otel")
+	//implementation("io.opentelemetry:opentelemetry-exporter-otlp")
 
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
 
