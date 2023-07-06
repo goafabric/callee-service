d5b258786c0c7833e3f63281145a19399848bbd8
Andreas Mautsch <andreas.mautsch@cgm.com>
2023-07-06 13:35:13 +0200

    changes

--- src/main/java/org/goafabric/calleeservice/extensions/ExceptionHandler.java
+++ src/main/java/org/goafabric/calleeservice/extensions/ExceptionHandler.java
@@ -5,25 +5,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandlerAdvice {
+public class ExceptionHandler {
     private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
-    @ExceptionHandler(IllegalArgumentException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
         log.warn(ex.getMessage(), ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.PRECONDITION_FAILED);
     }
 
-    @ExceptionHandler(IllegalStateException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalStateException ex) {
         log.warn(ex.getMessage(), ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.PRECONDITION_FAILED);
     }
 
-    @ExceptionHandler(Exception.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception ex) {
         log.error(ex.getMessage(), ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
