cdad1ab4b9d30cefdc5ac33afe2ea789b579820a
Andreas Mautsch <amautsch>
2023-07-21 08:16:28 +0200

    spring boot upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ jacoco.toolVersion = "0.8.9"
 plugins {
 	java
 	jacoco
-	id("org.springframework.boot") version "3.1.1"
+	id("org.springframework.boot") version "3.1.2"
 	id("io.spring.dependency-management") version "1.1.0"
 	id("org.graalvm.buildtools.native") version "0.9.23"
 	id("com.google.cloud.tools.jib") version "3.3.1"
