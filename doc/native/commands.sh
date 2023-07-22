7330ac3173d6b084c8b56ad0e49602755891ef4d
Andreas Mautsch <amautsch>
2023-07-22 10:29:01 +0200

    native builder

--- doc/native/commands.sh
+++
@@ -1,5 +0,0 @@
-gradle bootJar
-
-docker build -t native-image-builder ./doc/native && \
-docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/home/project/build \
-native-image-builder /bin/bash -c "/home/project/buildNativeImage.sh"
