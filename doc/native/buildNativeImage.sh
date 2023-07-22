027f68517ce1b1319433c85a1f51d7cdf84d4014
Andreas Mautsch <amautsch>
2023-07-22 09:47:53 +0200

    native builder

---
+++ doc/native/buildNativeImage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p /home/project/build/native/nativeCompile && cp /home/project/build/libs/*-SNAPSHOT.jar /home/project/build/native/nativeCompile && cd /home/project/build/native/nativeCompile && jar -xvf *-SNAPSHOT.jar
+[[ -f @META-INF/native-image/argfile ]] && echo !!using ARGFILE && ARGFILE=@META-INF/native-image/argfile; native-image -H:Name=myapplication -Ob $ARGFILE -cp .:BOOT-INF/classes:`find BOOT-INF/lib | tr '\n' ':'`
