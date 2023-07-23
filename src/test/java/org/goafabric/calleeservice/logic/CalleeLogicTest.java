5e662efae4c5191d63fd358052a98a39a1c3cbb1
Andreas Mautsch <amautsch>
2023-07-23 09:07:40 +0200

    changes

---
+++ src/test/java/org/goafabric/calleeservice/logic/CalleeLogicTest.java
@@ -0,0 +1,27 @@
+package org.goafabric.calleeservice.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalleeLogicTest {
+
+    @Test
+    void sayMyName() {
+        assertThat(new CalleeLogic().sayMyName("Heisenberg").message())
+                .isEqualTo("Your name is: Heisenberg");
+    }
+
+    @Test
+    void sayMyOtherName() {
+        assertThat(new CalleeLogic().sayMyOtherName("Slim Shady").message())
+                .isEqualTo("Your other name is: Slim Shady");
+    }
+
+    @Test
+    void save() {
+        assertThat(new CalleeLogic().sayMyOtherName("saved by the bell").message())
+                .isEqualTo("Storing your message: saved by the bell");
+    }
+}
