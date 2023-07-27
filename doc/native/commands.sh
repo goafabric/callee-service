c84d6e4a2a1ecf15fa00b4dd766d95bd1a6585dd
Andreas Mautsch <amautsch>
2023-07-27 15:45:52 +0200

    changes

--- doc/native/commands.sh
+++ doc/native/commands.sh
@@ -2,11 +2,11 @@ gradle clean bootJar
 
 docker build -t native-image-builder ./doc/native
 
-#docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/home/project/build \
-#native-image-builder /bin/bash -c "/home/project/buildNativeImage.sh"
+#docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/home/build \
+#native-image-builder /bin/bash -c "/home/buildNativeImage.sh"
 
-docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/home/project/build \
-native-image-builder /bin/bash -c 'mkdir -p /home/project/build/native/nativeCompile && cp /home/project/build/libs/*-SNAPSHOT.jar /home/project/build/native/nativeCompile && cd /home/project/build/native/nativeCompile && jar -xvf *-SNAPSHOT.jar \
+docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/home/build \
+native-image-builder /bin/bash -c 'mkdir -p /home/build/native/nativeCompile && cp /home/build/libs/*-SNAPSHOT.jar /home/build/native/nativeCompile && cd /home/build/native/nativeCompile && jar -xvf *-SNAPSHOT.jar \
 && [[ -f @META-INF/native-image/argfile ]] && echo !!using ARGFILE && ARGFILE=@META-INF/native-image/argfile; native-image -H:Name=application -Ob $ARGFILE -cp .:BOOT-INF/classes:$(find BOOT-INF/lib | tr "\n" ":")'
 
 
