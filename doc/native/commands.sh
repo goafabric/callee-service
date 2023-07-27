5d2e00df2a953cad47d6db67dac5a0f755b86fae
Andreas Mautsch <amautsch>
2023-07-27 15:51:48 +0200

    changes

--- doc/native/commands.sh
+++ doc/native/commands.sh
@@ -2,12 +2,12 @@ gradle clean bootJar
 
 docker build -t native-image-builder ./doc/native
 
-#docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/home/build \
-#native-image-builder /bin/bash -c "/home/buildNativeImage.sh"
+#docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/build \
+#native-image-builder /bin/bash -c "/buildNativeImage.sh"
 
-docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/home/build \
-native-image-builder /bin/bash -c 'mkdir -p /home/build/native/nativeCompile && cp /home/build/libs/*-SNAPSHOT.jar /home/build/native/nativeCompile && cd /home/build/native/nativeCompile && jar -xvf *-SNAPSHOT.jar \
-&& [[ -f @META-INF/native-image/argfile ]] && echo !!using ARGFILE && ARGFILE=@META-INF/native-image/argfile; native-image -H:Name=application -Ob $ARGFILE -cp .:BOOT-INF/classes:$(find BOOT-INF/lib | tr "\n" ":")'
+docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/build \
+native-image-builder /bin/bash -c 'mkdir -p /build/native/nativeCompile && cp /build/libs/*-SNAPSHOT.jar /build/native/nativeCompile && cd /build/native/nativeCompile && jar -xvf *-SNAPSHOT.jar \
+&& [[ -f @META-INF/native-image/argfile ]] && ARGFILE=@META-INF/native-image/argfile; native-image -H:Name=application -Ob $ARGFILE -cp .:BOOT-INF/classes:$(find BOOT-INF/lib | tr "\n" ":")'
 
 
 gradle jibNativeImage
