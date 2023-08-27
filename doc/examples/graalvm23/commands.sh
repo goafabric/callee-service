c2c71d8f3ad1380d1dbc9acfa184a8f92e8453b0
Andreas Mautsch <amautsch>
2023-08-27 08:14:55 +0200

    changes

---
+++ doc/examples/graalvm23/commands.sh
@@ -0,0 +1,14 @@
+gradle clean bootJar
+
+docker build -t native-image-builder ./doc/native
+
+#docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/build \
+#native-image-builder /bin/bash -c "/buildNativeImage.sh"
+
+docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/build \
+native-image-builder /bin/bash -c 'mkdir -p /build/native/nativeCompile && cp /build/libs/*-SNAPSHOT.jar /build/native/nativeCompile && cd /build/native/nativeCompile && jar -xvf *-SNAPSHOT.jar \
+&& native-image -H:Name=application -Ob $([[ -f META-INF/native-image/argfile ]] && echo @META-INF/native-image/argfile) -cp .:BOOT-INF/classes:$(find BOOT-INF/lib | tr "\n" ":")'
+
+
+gradle jibNativeImage
+
