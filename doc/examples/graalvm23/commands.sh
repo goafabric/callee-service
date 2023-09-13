7914a91a32b498367212c170459460c2bc1f0213
Andreas Mautsch <amautsch>
2023-09-13 10:24:19 +0200

    graalvm 23

--- doc/examples/graalvm23/commands.sh
+++ doc/examples/graalvm23/commands.sh
@@ -6,7 +6,7 @@ docker build -t native-image-builder ./doc/native
 #native-image-builder /bin/bash -c "/buildNativeImage.sh"
 
 docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/build \
-native-image-builder /bin/bash -c 'mkdir -p /build/native/nativeCompile && cp /build/libs/*-SNAPSHOT.jar /build/native/nativeCompile && cd /build/native/nativeCompile && jar -xvf *-SNAPSHOT.jar \
+ghcr.io/graalvm/native-image-community:17.0.8 /bin/bash -c 'mkdir -p /build/native/nativeCompile && cp /build/libs/*-SNAPSHOT.jar /build/native/nativeCompile && cd /build/native/nativeCompile && jar -xvf *-SNAPSHOT.jar \
 && native-image -H:Name=application -Ob -march=compatibility $([[ -f META-INF/native-image/argfile ]] && echo @META-INF/native-image/argfile) -cp .:BOOT-INF/classes:$(find BOOT-INF/lib | tr "\n" ":")'
 
 
