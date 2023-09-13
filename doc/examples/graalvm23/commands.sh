cbc7d174512679191799a48369cec2bf4f977ccc
Andreas Mautsch <amautsch>
2023-09-13 11:39:10 +0200

    graalvm 23

--- doc/examples/graalvm23/commands.sh
+++ doc/examples/graalvm23/commands.sh
@@ -1,5 +1,4 @@
-gradle clean bootJar
-
+gradle clean bootJar && \
 docker run --rm --name native-image-builder --mount type=bind,source=$(pwd)/build,target=/build --entrypoint /bin/bash ghcr.io/graalvm/native-image-community:17.0.8 \
 -c 'mkdir -p /build/native/nativeCompile && cp /build/libs/*-SNAPSHOT.jar /build/native/nativeCompile && cd /build/native/nativeCompile && jar -xvf *-SNAPSHOT.jar \
 && native-image -H:Name=application -J-Xmx5000m -Ob -march=compatibility $([[ -f META-INF/native-image/argfile ]] && echo @META-INF/native-image/argfile) -cp .:BOOT-INF/classes:$(ls -d -1 "$PWD/BOOT-INF/lib/"*.* | tr "\n" ":")'
