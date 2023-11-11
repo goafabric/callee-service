e2cd59a333a482aca66718e81af232ee3f1d1e56
Andreas Mautsch <amautsch>
2023-11-11 17:54:40 +0100

    jib changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,7 +79,6 @@ tasks.register("dockerImageNativeNoTest") {group = "build"; dependsOn("bootJar")
 		jib.from.image = "ubuntu:22.04"
 		jib.to.image = "${dockerRegistry}/${project.name}-native" + (if (System.getProperty("os.arch").equals("aarch64")) "-arm64v8" else "") + ":${project.version}"
 		jib.pluginExtensions { pluginExtension {properties = mapOf("imageName" to "application"); implementation = "com.google.cloud.tools.jib.gradle.extension.nativeimage.JibNativeImageExtension" }}
-		jib.from.platforms.set(null as Iterable<com.google.cloud.tools.jib.gradle.PlatformParameters>)
 	}
 	finalizedBy("jib")
 }
