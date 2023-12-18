93bc8003aedeedd18f10558790ea9918a7545a2a
Andreas Mautsch <amautsch>
2023-12-18 13:44:12 +0100

    changes

--- doc/examples/graalvm23/gradle_paketo.kts
+++ doc/examples/graalvm23/gradle_paketo.kts
@@ -1,4 +1,4 @@
-val nativeBuilder = "dashaun/builder:20231114"
+val nativeBuilder = "dashaun/builder:20231204"
 tasks.register("dockerImageNative") { group = "build"; dependsOn("bootBuildImage") }
 tasks.named<BootBuildImage>("bootBuildImage") {
     val nativeImageName = "${dockerRegistry}/${project.name}-native" + (if (System.getProperty("os.arch").equals("aarch64")) "-arm64v8" else "") + ":${project.version}"
