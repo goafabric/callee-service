022d4be7c0ccb5e278502ade70ca10bec34d0d41
Andreas Mautsch <amautsch>
2024-03-01 07:59:30 +0100

    roles all

--- doc/examples/roles-allowed/HttpInterceptor.java
+++ doc/examples/roles-allowed/HttpInterceptor.java
@@ -20,28 +20,37 @@ import java.util.List;
 
 
 //add to existing HttpInterceptor
-public class HttpInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        tenantId.set(request.getHeader("X-TenantId"));
-        userName.set(request.getHeader("X-Auth-Request-Preferred-Username"));
-        configureLogsAndTracing(request);
+@Override
+public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    tenantId.set(request.getHeader("X-TenantId"));
+    userName.set(request.getHeader("X-Auth-Request-Preferred-Username"));
+    configureLogsAndTracing(request);
 
-        if (handler instanceof HandlerMethod) {
-            log.info(" {} method called for user {} ", ((HandlerMethod) handler).getShortLogMessage(), getUserName());
-            checkRoles((HandlerMethod) handler, getUserName());
-        }
-        return true;
-    }
+    if (handler instanceof HandlerMethod) {
+        log.info(" {} method called for user {} ", ((HandlerMethod) handler).getShortLogMessage(), getUserName());
 
-    private static void checkRoles(HandlerMethod handler, String userName) {
-        List<String> methodRoles = Arrays.asList(handler.getMethodAnnotation(RolesAllowed.class).value());
-        List<String> userRoles = Arrays.asList("READ", "WRITE", "ADDRESS_DELETE"); //TODO: getBackendRoles(userName) => read roles via DB or REST with every call if not cached
-        methodRoles.stream().filter(role -> !userRoles.contains(role)).findAny().ifPresent(role -> {
-            throw new IllegalStateException("not allowed");
-        });
+        checkOrganizations(request.getHeader("X-OrganizationId"), getUserName());
+        checkRoles((HandlerMethod) handler, getUserName());
+        //the 2 calls could be merged into one for optimization, as its basically just 2 groups of roles
     }
+    return true;
 }
+
+private static void checkOrganizations(String organizationId, String userName) {
+    List<String> userOrganizationIds = Arrays.asList("5", "7"); //TODO: getRoles(userName, type = "organization") => read roles via DB or REST with every call if not cached
+    userOrganizationIds.stream().filter(oid -> !userOrganizationIds.contains(oid)).findAny().ifPresent(oid -> {
+        throw new IllegalStateException("not eligable for organization");
+    });
+}
+
+private static void checkRoles(HandlerMethod handler, String userName) {
+    List<String> methodRoles = Arrays.asList(handler.getMethodAnnotation(RolesAllowed.class).value());
+    List<String> userRoles = Arrays.asList("READ", "WRITE", "ADDRESS_DELETE"); //TODO: getRoles(userName, type = "backend") => read roles via DB or REST with every call if not cached
+    methodRoles.stream().filter(role -> !userRoles.contains(role)).findAny().ifPresent(role -> {
+        throw new IllegalStateException("not allowed");
+    });
+}
+
 
 //Example implementation
 public class SomethinController() {
