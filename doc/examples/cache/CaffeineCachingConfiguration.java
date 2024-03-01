6ea68c6881702e2855d2f5fc9ac802a52f38a992
Andreas Mautsch <amautsch>
2024-03-01 09:14:03 +0100

    changes

---
+++ doc/examples/cache/CaffeineCachingConfiguration.java
@@ -0,0 +1,62 @@
+package org.goafabric.calleeservice.cache;
+
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleKey;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
+
+import java.util.concurrent.TimeUnit;
+
+//implementation("com.github.ben-manes.caffeine:caffeine"); implementation("org.springframework.boot:spring-boot-starter-cache");
+
+@Configuration
+@EnableCaching
+@ImportRuntimeHints(CaffeineCachingConfiguration.CacheRuntimeHints.class)
+public class CaffeineCachingConfiguration extends CachingConfigurerSupport {
+
+    private Long cacheMaxSize = 1000l;
+
+    private Long cacheExpiry = 10l;
+
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        final CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .maximumSize(cacheMaxSize)
+                .expireAfterAccess(cacheExpiry, TimeUnit.MINUTES));
+        return cacheManager;
+    }
+
+    @Bean
+    @Override
+    public KeyGenerator keyGenerator() {
+        return (target, method, params) -> {
+            var tenantId = "0"; //HttpInterceptor.getTenantID()
+            var organizationId = "1"; //HttpInterceptor.getOrganizationId()
+            return new SimpleKey(tenantId, organizationId, method.getName(), params);
+        };
+    }
+
+    static class CacheRuntimeHints implements RuntimeHintsRegistrar {
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            try { //caffeine hints
+                hints.reflection().registerType(Class.forName("com.github.benmanes.caffeine.cache.SSMSA"), MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+                hints.reflection().registerType(Class.forName("com.github.benmanes.caffeine.cache.PSAMS"), MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ doc/examples/cache/RedisCachingConfiguration.java
@@ -0,0 +1,46 @@
+package org.goafabric.calleeservice.cache;
+
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleKey;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+//implementation("org.springframework.boot:spring-boot-starter-data-redis"); implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml"); implementation("org.springframework.boot:spring-boot-starter-cache");
+//docker run --name redis --rm -p6379:6379 -p16379:16379 redis:7.2.1
+
+@Configuration
+@EnableCaching
+//@RegisterReflectionForBinding({TestComponent.Foo.class, TestComponent.Call.class, TestComponent.Bar.class}) //classes that are cached need to be registered for Reflection
+public class RedisCachingConfiguration extends CachingConfigurerSupport {
+
+    private Long cacheMaxSize = 1000l;
+
+    private Long cacheExpiry = 10l;
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(cacheExpiry))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+
+    @Bean
+    @Override
+    public KeyGenerator keyGenerator() {
+        return (target, method, params) -> {
+            var tenantId = "0"; //HttpInterceptor.getTenantID()
+            var organizationId = "1"; //HttpInterceptor.getOrganizationId()
+            return new SimpleKey(tenantId, organizationId, method.getName(), params);
+        };
+    }
+
+}
