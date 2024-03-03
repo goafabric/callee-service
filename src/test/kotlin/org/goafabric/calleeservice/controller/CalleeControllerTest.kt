b60db7ed146e5b7be2526ad67776357da83a0bb1
Andreas Mautsch <amautsch>
2024-03-03 09:46:49 +0100

    tracing fixed

--- src/test/kotlin/org/goafabric/calleeservice/controller/CalleeControllerTest.kt
+++ src/test/kotlin/org/goafabric/calleeservice/controller/CalleeControllerTest.kt
@@ -1,11 +1,12 @@
-/*
 package org.goafabric.calleeservice.controller
 
 import org.assertj.core.api.Assertions.assertThat
 import org.goafabric.calleeservice.logic.CalleeLogic
 import org.junit.jupiter.api.Test
-import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+
 
 internal class CalleeControllerTest {
     private val calleeLogic: CalleeLogic = Mockito.mock(CalleeLogic::class.java)
@@ -13,14 +14,14 @@ internal class CalleeControllerTest {
 
     @Test
     fun sayMyName() {
-        Mockito.`when`(calleeLogic.sayMyName(ArgumentMatchers.eq("Heisenberg"))).thenReturn(Callee("", "Heisenberg"))
+        Mockito.`when`(calleeLogic.sayMyName(eq("Heisenberg"))).thenReturn(Callee("", "Heisenberg"))
         assertThat(calleController.sayMyName("Heisenberg").message)
             .isEqualTo("Heisenberg")
     }
 
     @Test
     fun sayMyOtherName() {
-        Mockito.`when`(calleeLogic.sayMyOtherName(ArgumentMatchers.eq("Slim Shady")))
+        Mockito.`when`(calleeLogic.sayMyOtherName(eq("Slim Shady")))
             .thenReturn(Callee("", "Slim Shady"))
         assertThat(calleController.sayMyOtherName("Slim Shady").message)
             .isEqualTo("Slim Shady")
@@ -28,11 +29,9 @@ internal class CalleeControllerTest {
 
     @Test
     fun save() {
-        Mockito.`when`(calleeLogic.save(ArgumentMatchers.any(Callee::class.java))).thenReturn(Callee("", "saved"))
+        Mockito.`when`(calleeLogic.save(any())).thenReturn(Callee("", "saved"))
         assertThat(calleController.save(Callee("", "saved"))!!.message)
             .isEqualTo("saved")
         Mockito.verify(calleeLogic, Mockito.times(1)).save(Callee("", "saved"))
     }
 }
-
- */
