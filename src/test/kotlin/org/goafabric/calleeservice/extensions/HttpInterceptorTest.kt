51e0cc60d522b6760466c04660999618abc63cef
Andreas Mautsch <amautsch>
2024-03-03 10:03:45 +0100

    tests added

---
+++ src/test/kotlin/org/goafabric/calleeservice/extensions/HttpInterceptorTest.kt
@@ -0,0 +1,32 @@
+package org.goafabric.calleeservice.extensions
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+
+internal class HttpInterceptorTest {
+    private val httpInterceptor = HttpInterceptor()
+
+    @Test
+    fun preHandle() {
+        assertThat(
+            httpInterceptor.preHandle(
+                mock(HttpServletRequest::class.java),
+                mock(HttpServletResponse::class.java), Any()
+            )
+        ).isTrue()
+    }
+
+    @Test
+    fun getTenantId() {
+        assertThat(HttpInterceptor.getTenantId()).isEqualTo("0")
+    }
+
+    @Test
+    fun getUserName() {
+        assertThat(HttpInterceptor.getUserName()).isEqualTo("")
+    }
+
+}
---
+++ src/test/kotlin/org/goafabric/calleeservice/logic/CalleeLogicTest.kt
@@ -0,0 +1,27 @@
+package org.goafabric.calleeservice.logic
+
+import org.assertj.core.api.Assertions.assertThat
+import org.goafabric.calleeservice.controller.Callee
+import org.junit.jupiter.api.Test
+
+internal class CalleeLogicTest {
+    private val calleeLogic = CalleeLogic()
+
+    @Test
+    fun sayMyName() {
+        assertThat(calleeLogic.sayMyName("Heisenberg").message)
+            .isEqualTo("Your name is: Heisenberg")
+    }
+
+    @Test
+    fun sayMyOtherName() {
+        assertThat(calleeLogic.sayMyOtherName("Slim Shady").message)
+            .isEqualTo("Your other name is: Slim Shady")
+    }
+
+    @Test
+    fun save() {
+        assertThat(calleeLogic.save(Callee("0", "saved by the bell"))!!.message)
+            .isEqualTo("Storing your message: saved by the bell")
+    }
+}
