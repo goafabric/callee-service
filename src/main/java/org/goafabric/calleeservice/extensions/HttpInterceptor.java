7978ce9c4e753abf454da4786db15391a6186fe2
Andreas Mautsch <amautsch>
2024-03-08 17:04:26 +0100

    kotlin added

--- src/main/java/org/goafabric/calleeservice/extensions/HttpInterceptor.java
+++ src/main/java/org/goafabric/calleeservice/extensions/HttpInterceptor.java
@@ -14,11 +14,20 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Map;
+
 
 public class HttpInterceptor implements HandlerInterceptor {
+    record TenantContext(String tenantId, String organizationId, String userName) {
+        public Map<String, String> toMap() {
+            return Map.of("X-TenantId", tenantId, "X-OrganizationId", organizationId, "X-UserName", userName);
+        }
+        //public String tenantId() { return tenantId != null ? tenantId : "0"; }
+        //public String organizationId() { return organizationId != null ? organizationId : "0"; }
+    }
+
     private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
-    private static final ThreadLocal<String> tenantId = new ThreadLocal<>();
-    private static final ThreadLocal<String> userName = new ThreadLocal<>();
+    private static final ThreadLocal<TenantContext> tenantContext = ThreadLocal.withInitial(() -> new TenantContext(null, null, null));
 
     @Configuration
     static class Configurer implements WebMvcConfigurer {
@@ -30,8 +39,9 @@ public class HttpInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        tenantId.set(request.getHeader("X-TenantId"));
-        userName.set(request.getHeader("X-Auth-Request-Preferred-Username"));
+        tenantContext.set(new TenantContext(
+                request.getHeader("X-TenantId"), request.getHeader("X-OrganizationId"), request.getHeader("X-Auth-Request-Preferred-Username")));
+        
         configureLogsAndTracing(request);
 
         if (handler instanceof HandlerMethod) {
@@ -42,8 +52,7 @@ public class HttpInterceptor implements HandlerInterceptor {
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        tenantId.remove();
-        userName.remove();
+        tenantContext.remove();
         MDC.remove("tenantId");
     }
 
@@ -54,15 +63,18 @@ public class HttpInterceptor implements HandlerInterceptor {
     }
 
     public static String getTenantId() {
-        return tenantId.get() != null ? tenantId.get() : "0"; //tdo
+        return tenantContext.get().tenantId != null ? tenantContext.get().tenantId() : "0"; //tdo
     }
 
     public static String getUserName() {
-        return userName.get() != null ? userName.get()
+        return tenantContext.get().userName != null ? tenantContext.get().userName
                 : SecurityContextHolder.getContext().getAuthentication() != null ? SecurityContextHolder.getContext().getAuthentication().getName() : "";
     }
 
+    /*
     public static void setTenantId(String tenant) {
         tenantId.set(tenant);
     }
+
+     */
 }
