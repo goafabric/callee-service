c862e498dc960e7124e8c7c1b860b4eab8cd5b95
Andreas Mautsch <amautsch>
2024-03-10 10:44:06 +0100

    tenantcontext added

--- src/main/kotlin/org/goafabric/calleeservice/extensions/HttpInterceptor.kt
+++ src/main/kotlin/org/goafabric/calleeservice/extensions/HttpInterceptor.kt
@@ -3,11 +3,11 @@ package org.goafabric.calleeservice.extensions
 import io.micrometer.common.KeyValue
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.server.observation.ServerRequestObservationContext
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.ServerHttpObservationFilter
 import org.springframework.web.method.HandlerMethod
 import org.springframework.web.servlet.HandlerInterceptor
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 class HttpInterceptor : HandlerInterceptor {
-    private val log = LoggerFactory.getLogger(this.javaClass.name)
+    private val log: Logger = LoggerFactory.getLogger(this.javaClass.name)
 
     @Configuration
     internal class Configurer : WebMvcConfigurer {
@@ -25,11 +25,11 @@ class HttpInterceptor : HandlerInterceptor {
     }
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        tenantId.set(request.getHeader("X-TenantId"))
-        userName.set(request.getHeader("X-Auth-Request-Preferred-Username"))
+        TenantContext.setContext(request)
         configureLogsAndTracing(request)
+
         if (handler is HandlerMethod) {
-            log.info(" {} method called for user {} ", handler.shortLogMessage, getUserName())
+            log.info(" {} method called for user {} ", handler.shortLogMessage, TenantContext.userName)
         }
         return true
     }
@@ -40,33 +40,19 @@ class HttpInterceptor : HandlerInterceptor {
         handler: Any,
         ex: Exception?
     ) {
-        tenantId.remove()
+        TenantContext.removeContext()
         MDC.remove("tenantId")
     }
 
     companion object {
-        private val tenantId = ThreadLocal<String?>()
-        private val userName = ThreadLocal<String?>()
         private fun configureLogsAndTracing(request: HttpServletRequest) {
-            MDC.put("tenantId", getTenantId())
+            MDC.put("tenantId", TenantContext.tenantId)
             ServerHttpObservationFilter.findObservationContext(request)
                 .ifPresent { context: ServerRequestObservationContext ->
                     context.addHighCardinalityKeyValue(
-                        KeyValue.of("tenant.id", getTenantId())
+                        KeyValue.of("tenant.id", TenantContext.tenantId)
                     )
                 }
-        }
-
-        fun getTenantId(): String {
-            return if (tenantId.get() != null) tenantId.get()!! else "0" //tdo
-        }
-
-        fun getUserName(): String {
-            return if (userName.get() != null) userName.get()!! else if (SecurityContextHolder.getContext().authentication != null) SecurityContextHolder.getContext().authentication.name else ""
-        }
-
-        fun setTenantId(tenant: String?) {
-            tenantId.set(tenant)
         }
     }
 }
