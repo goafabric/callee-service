b57c310e1b14357ff2bbba267356111540ac7066
Andreas Mautsch <amautsch>
2024-04-29 08:01:40 +0200

    tenantcontext fixed

--- src/test/java/org/goafabric/calleeservice/extensions/TenantContextTest.java
+++ src/test/java/org/goafabric/calleeservice/extensions/TenantContextTest.java
@@ -8,36 +8,44 @@ class TenantContextTest {
 
     @Test
     void getTenantId() {
-        TenantContext.setContext(new TenantContext.TenantContextRecord(null, null, null));
+        TenantContext.setContext(null, null, null, null);
         assertThat(TenantContext.getTenantId()).isEqualTo("0");
     }
 
     @Test
     void getOrganizationId() {
-        TenantContext.setContext(new TenantContext.TenantContextRecord(null, null, null));
+        TenantContext.setContext(null, null, null, null);
         assertThat(TenantContext.getOrganizationId()).isEqualTo("0");
     }
 
     @Test
     void getUserName() {
-        TenantContext.setContext(new TenantContext.TenantContextRecord(null, null, null));
+        TenantContext.setContext(null, null, null, null);
         assertThat(TenantContext.getUserName()).isEqualTo("anonymous");
     }
     
     @Test
     void setTenantId() {
-        TenantContext.setContext(new TenantContext.TenantContextRecord(null, null, null));
+        TenantContext.setContext(null, null, null, null);
         TenantContext.setTenantId("42");
         assertThat(TenantContext.getTenantId()).isEqualTo("42");
     }
 
     @Test
+    void getUserNameFromUserINfo() {
+        var userInfo = "eyJwcmVmZXJyZWRfdXNlcm5hbWUiOiJqb2huIGRvZSIsImFsZyI6IkhTMjU2In0";
+        TenantContext.setContext("42", "44", "user", userInfo);
+        assertThat(TenantContext.getUserName()).isEqualTo("john doe");
+    }
+
+    @Test
     void getAll() {
-        TenantContext.setContext(new TenantContext.TenantContextRecord("42", "44", "user"));
+        TenantContext.setContext("42", "44", "user", null);
         assertThat(TenantContext.getTenantId()).isEqualTo("42");
         assertThat(TenantContext.getOrganizationId()).isEqualTo("44");
         assertThat(TenantContext.getUserName()).isEqualTo("user");
         assertThat(TenantContext.getAdapterHeaderMap()).isNotNull().isNotEmpty();
     }
+
 
 }
