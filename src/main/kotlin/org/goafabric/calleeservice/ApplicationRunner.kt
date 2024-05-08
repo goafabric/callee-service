836bad570439d041d0a47cfd0f2fa8465ff876b6
Andreas Mautsch <amautsch>
2024-05-08 07:29:38 +0200

    spring security readded

--- src/main/kotlin/org/goafabric/calleeservice/ApplicationRunner.kt
+++ src/main/kotlin/org/goafabric/calleeservice/ApplicationRunner.kt
@@ -9,6 +9,9 @@ import org.springframework.boot.SpringApplication
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class ApplicationRunner {
@@ -19,5 +22,26 @@ class ApplicationRunner {
                 SpringApplication.exit(context, ExitCodeGenerator { 0 })
             }
         }
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(
+        http: HttpSecurity,
+        @Value("\${security.authentication.enabled:true}") isAuthenticationEnabled: Boolean
+    ): SecurityFilterChain? {
+        return if (isAuthenticationEnabled) http.authorizeHttpRequests { auth ->
+            auth.requestMatchers("/actuator/**").permitAll().anyRequest().authenticated()
+        }
+            .httpBasic { }
+            .csrf { csrf: CsrfConfigurer<HttpSecurity> -> csrf.disable() }
+            .build()
+        else http.authorizeHttpRequests { auth -> auth.anyRequest().permitAll() }.build()
+    }
+
+
+    @Bean
+    fun disableHttpServerObservationsFromName(): ObservationPredicate? {
+        return ObservationPredicate { name: String, _: Observation.Context? -> !name.startsWith("spring.security.") }
     }
 }
