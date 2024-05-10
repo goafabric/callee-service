1de060289baceb4d530c3efa276f91963c34ccfa
Andreas Mautsch <amautsch>
2024-05-10 10:20:32 +0200

    noop encoder

--- doc/examples/oidc/SecurityConfig.java
+++ doc/examples/oidc/SecurityConfig.java
@@ -2,6 +2,7 @@
 public SecurityFilterChain filterChain(HttpSecurity http, @Value("${security.authentication.enabled:true}") Boolean isAuthenticationEnabled, HandlerMappingIntrospector introspector) throws Exception {
     return isAuthenticationEnabled
             ? http.authorizeHttpRequests(auth -> auth.requestMatchers(new MvcRequestMatcher(introspector, "/actuator/**")).permitAll().anyRequest().authenticated())
+            //.sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.NEVER))
             .oauth2Login(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable).build()
             : http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).httpBasic(httpBasic -> {}).csrf(AbstractHttpConfigurer::disable).build();
 }
