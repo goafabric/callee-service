242011d6ab6475f56cfa0f859034d7cf61ef7f95
Andreas Mautsch <amautsch>
2024-05-14 07:48:26 +0200

    changes

--- src/main/kotlin/org/goafabric/calleeservice/Application.kt
+++ src/main/kotlin/org/goafabric/calleeservice/Application.kt
@@ -1,10 +1,22 @@
 package org.goafabric.calleeservice
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.ExitCodeGenerator
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
 
 @SpringBootApplication
-class Application
+class Application(@Autowired private val applicationContext: ApplicationContext):
+    CommandLineRunner {
+    override fun run(vararg args: String?) {
+        if (args.isNotEmpty() && "-check-integrity" == args[0]) {
+            SpringApplication.exit(applicationContext, ExitCodeGenerator { 0 })
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
