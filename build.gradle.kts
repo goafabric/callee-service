1197329ea0c6fe78a56c24340d8366ba56769122
Andreas Mautsch <amautsch>
2024-05-31 11:32:04 +0200

    buildpack native

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,7 @@ jib {
 tasks.register("dockerImageNative") { description= "Native Image"; group = "build"; dependsOn("bootBuildImage") }
 tasks.named<BootBuildImage>("bootBuildImage") {
 	val nativeImageName = "${dockerRegistry}/${project.name}-native" + (if (System.getProperty("os.arch").equals("aarch64")) "-arm64v8" else "") + ":${project.version}"
-	builder.set("paketobuildpacks/builder-jammy-buildpackless-tiny") //builder.set("paketobuildpacks/builder:tiny")
+	builder.set("paketobuildpacks/builder-jammy-buildpackless-tiny")
 	buildpacks.add(nativeBuilder)
 	imageName.set(nativeImageName)
 	environment.set(mapOf("BP_NATIVE_IMAGE" to "true", "BP_JVM_VERSION" to "21", "BP_NATIVE_IMAGE_BUILD_ARGUMENTS" to "-J-Xmx5000m -march=compatibility"))
