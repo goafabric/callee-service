7f958efdf46a98b225b8b9b5d99319fc76f10737
Andreas Mautsch <amautsch>
2024-06-28 18:23:29 +0200

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,10 +69,11 @@ jib {
 	from.platforms.set(listOf(amd64, arm64))
 }
 
-tasks.register("dockerImageNative") { group = "build"; dependsOn("bootBuildImage") }
+tasks.register("dockerImageNative") { description= "Native Image"; group = "build"; dependsOn("bootBuildImage") }
 tasks.named<BootBuildImage>("bootBuildImage") {
 	val nativeImageName = "${dockerRegistry}/${project.name}-native" + (if (System.getProperty("os.arch").equals("aarch64")) "-arm64v8" else "") + ":${project.version}"
-	builder.set(nativeBuilder)
+	builder.set("paketobuildpacks/builder-jammy-buildpackless-tiny")
+	buildpacks.add(nativeBuilder)
 	imageName.set(nativeImageName)
 	environment.set(mapOf("BP_NATIVE_IMAGE" to "true", "BP_JVM_VERSION" to "21", "BP_NATIVE_IMAGE_BUILD_ARGUMENTS" to "-J-Xmx5000m -march=compatibility"))
 	doLast {
