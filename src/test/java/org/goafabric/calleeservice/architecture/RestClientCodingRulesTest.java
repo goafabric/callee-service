09f47fcc417a446154f91efd8acce369f35f96b1
Andreas Mautsch <amautsch>
2024-07-20 17:19:51 +0200

    changes

--- src/test/java/org/goafabric/calleeservice/architecture/RestClientCodingRulesTest.java
+++ src/test/java/org/goafabric/calleeservice/architecture/RestClientCodingRulesTest.java
@@ -4,7 +4,6 @@ import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.service.annotation.HttpExchange;
 
@@ -25,7 +24,9 @@ class RestClientCodingRulesTest {
             .haveFullyQualifiedName("org.springframework.web.client.RestTemplate")
             .orShould().dependOnClassesThat()
             .haveFullyQualifiedName("org.springframework.web.reactive.function.client.WebClient")
-            .as("Only use the declarative REST Client, as otherwise native image support is broken");
+            .as("Only use the declarative REST Client, as otherwise native image support is broken")
+            .allowEmptyShould(true);
+
 
     @ArchTest
     static final ArchRule declarative_client_should_use_circuit_breaker =
@@ -33,6 +34,6 @@ class RestClientCodingRulesTest {
             .areMetaAnnotatedWith(HttpExchange.class)
             .should()
             .beDeclaredInClassesThat()
-            .areMetaAnnotatedWith(CircuitBreaker.class)
+            .areMetaAnnotatedWith("io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker")
             .allowEmptyShould(true);
 }
