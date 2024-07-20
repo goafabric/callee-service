a46b9dca691bdaddb3568e670acd765ed5c05f6d
Andreas Mautsch <amautsch>
2024-07-20 17:58:49 +0200

    changes

--- src/test/java/org/goafabric/calleeservice/architecture/LayerCodingRulesTest.java
+++ src/test/java/org/goafabric/calleeservice/architecture/LayerCodingRulesTest.java
@@ -2,14 +2,20 @@ package org.goafabric.calleeservice.architecture;
 
 
 import com.tngtech.archunit.base.DescribedPredicate;
+import com.tngtech.archunit.core.domain.JavaMethod;
 import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.ConditionEvents;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.simpleNameStartingWith;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 @AnalyzeClasses(packages = "org.goafabric", importOptions = DoNotIncludeTests.class)
@@ -38,4 +44,18 @@ class LayerCodingRulesTest {
             .that().resideInAPackage("..dto")
             .should().beRecords()
             .because("Classes in the dto package should be records");
+
+    @ArchTest
+    static final ArchRule controller_return = methods()
+            .that().areDeclaredInClassesThat().areAnnotatedWith(RestController.class)
+            .should(new ArchCondition<>("ResponseEntity check") {
+                @Override
+                public void check(JavaMethod method, ConditionEvents events) {
+                    if (method.getRawReturnType().isAssignableFrom(ResponseEntity.class)) {
+                        String message = String.format("Method %s in %s returns ResponseEntity",
+                                method.getName(), method.getOwner().getName());
+                        events.add(SimpleConditionEvent.violated(method, message));
+                    }
+                }
+            });
 }
