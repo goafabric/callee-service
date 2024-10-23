5d873d640f44b4ac93777e346870c3b0364de628
Andreas Mautsch <amautsch>
2024-10-23 14:32:04 +0200

    changes

--- src/main/java/org/goafabric/calleeservice/s3/ObjectStorageAzure.java
+++ src/main/java/org/goafabric/calleeservice/s3/ObjectStorageAzure.java
@@ -10,52 +10,67 @@ import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.util.List;
 
 @Service
 public class ObjectStorageAzure {
 
-    private final BlobContainerClient blobContainerClient;
+    private final String accountName;
+    private final String accountKey;
 
     public ObjectStorageAzure(@Value("${azure.storage.account-name}") String accountName,
                               @Value("${azure.storage.account-key}") String accountKey) {
-        String endpoint = String.format("https://%s.blob.core.windows.net", accountName);
-        this.blobContainerClient = new BlobContainerClientBuilder()
-                .endpoint(endpoint)
-                .credential(new StorageSharedKeyCredential(accountName, accountKey))
-                .containerName(getBucketName())
-                .buildClient();
+        this.accountName = accountName;
+        this.accountKey = accountKey;
     }
 
     public ObjectEntry getById(String key) {
         var outputStream = new ByteArrayOutputStream();
-        blobContainerClient.getBlobClient(key).downloadStream(outputStream);
+        getBobClient().getBlobClient(key).downloadStream(outputStream);
         return new ObjectEntry(key, "TODO", (long) outputStream.toByteArray().length, outputStream.toByteArray());
     }
 
     public void save(ObjectEntry objectEntry) {
-        blobContainerClient.getServiceClient().createBlobContainerIfNotExists(getBucketName());
-        blobContainerClient.getBlobClient(objectEntry.objectName())
+        getBobClient().getServiceClient().createBlobContainerIfNotExists(getBucketName());
+        getBobClient().getBlobClient(objectEntry.objectName())
             .upload(new ByteArrayInputStream(objectEntry.data()), objectEntry.objectSize());
     }
 
+    public List<ObjectEntry> search(String search) {
+        return getBobClient().listBlobs().stream()
+                .filter(item -> item.getName().toLowerCase().startsWith(search))
+                .map(item -> getById(item.getName()))
+                .toList();
+    }
+
+    /*
     @PostConstruct
     public void demo() {
         System.out.println(getById("Cletus.png"));
     }
 
-    //    @PostConstruct
-//    public void demo() {
-//        save(
-//                new ObjectEntry("hello_world.txt", "text/plain",
-//                        Long.valueOf("hello world".length()), "hello world".getBytes()));
-//        var objectEntry = getById("hello_world.txt");
-//        System.err.println("getById : " + objectEntry);
-//        search("hello").stream().forEach(s -> System.err.println("fromlist : " + s.toString()));
-//    }
+     */
 
+    @PostConstruct
+    public void demo() {
+        save(
+                new ObjectEntry("hello_world.txt", "text/plain",
+                        Long.valueOf("hello world".length()), "hello world".getBytes()));
+        var objectEntry = getById("hello_world.txt");
+        System.err.println("getById : " + objectEntry);
+        search("hello").stream().forEach(s -> System.err.println("fromlist : " + s.toString()));
+    }
+
+    public BlobContainerClient getBobClient() {
+        return new BlobContainerClientBuilder()
+                .endpoint(String.format("https://%s.blob.core.windows.net", accountName))
+                .credential(new StorageSharedKeyCredential(accountName, accountKey))
+                .containerName(getBucketName())
+                .buildClient();
+    }
 
     public String getBucketName() {
-        return "mybucket";
+        return "tenant-5";
     }
 }
 
