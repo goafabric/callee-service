46aef6587c207360568a24bcc4f243d5079d9258
Andreas Mautsch <amautsch>
2024-10-25 09:05:40 +0200

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,8 @@ repositories {
 
 dependencies {
 	constraints {
-		annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
-		implementation("org.mapstruct:mapstruct:1.5.5.Final")
+		annotationProcessor("org.mapstruct:mapstruct-processor:1.6.2")
+		implementation("org.mapstruct:mapstruct:1.6.2")
 		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 		implementation("io.github.resilience4j:resilience4j-spring-boot3:2.1.0")
 		implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:1.0.3")
