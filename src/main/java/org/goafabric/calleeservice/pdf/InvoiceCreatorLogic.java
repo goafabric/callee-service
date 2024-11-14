233e2d8746513a8d00d822394304cc530da8c43c
Andreas Mautsch <amautsch>
2024-11-14 16:00:48 +0100

    pdf added

--- src/main/java/org/goafabric/calleeservice/pdf/InvoiceCreatorLogic.java
+++ src/main/java/org/goafabric/calleeservice/pdf/InvoiceCreatorLogic.java
@@ -5,9 +5,14 @@
 //import com.lowagie.text.pdf.PdfPTable;
 //import com.lowagie.text.pdf.PdfWriter;
 //import com.lowagie.text.pdf.draw.LineSeparator;
+//import jakarta.annotation.PostConstruct;
 //import org.springframework.stereotype.Component;
 //
 //import java.io.ByteArrayOutputStream;
+//import java.io.File;
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.util.Arrays;
 //
 //@Component
 //public class InvoiceCreatorLogic {
@@ -177,5 +182,25 @@
 //        amountCell.setBorder(Rectangle.NO_BORDER);
 //        table.addCell(amountCell);
 //    }
+//
+//
+//    @PostConstruct
+//    public void createInvoiceFile() throws IOException {
+//        Files.write(new File("invoice.pdf").toPath(), createInvoice(createInvoiceData()));
+//    }
+//
+//    private static InvoiceCreatorLogic.InvoiceData createInvoiceData() {
+//        var organization = new InvoiceCreatorLogic.Organization("Practice Dr. Hibbert", Arrays.asList("Hibbertstreet 123", "443 Shelbyville"));
+//        var patient = new InvoiceCreatorLogic.Patient("Homer", "Simpson", Arrays.asList("Mr", "Homer Simpson", "Evergreen Terrace 742", "443 Springfield"));
+//        var invoiceItems = Arrays.asList(
+//                new InvoiceCreatorLogic.ChargeItem("", 1, "5", "2,300", "Examination System", 10.72),
+//                new InvoiceCreatorLogic.ChargeItem("", 1, "7", "2,300", "Examination Body", 21.46),
+//                new InvoiceCreatorLogic.ChargeItem("", 1, "5", "1,800", "Wrapping Bandages", 4.19));
+//        var textBlockIntro = "Dear Mr. Simpson,\nfor my work i would like to charge you with the following items:";
+//        var textBlockOutro = "Please pay the until the 01.01.2031 refering the invoice number";
+//        return new InvoiceCreatorLogic.InvoiceData(organization, patient, "E66.00 Adipositas",
+//                invoiceItems, textBlockIntro, textBlockOutro);
+//    }
+//
 //
 //}
