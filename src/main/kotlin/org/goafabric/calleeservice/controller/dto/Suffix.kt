778d3459c56f820eb55eb25617cd4633135e9ea6
Andreas Mautsch <amautsch>
2025-01-08 11:59:42 +0100

    enum test

---
+++ src/main/kotlin/org/goafabric/calleeservice/controller/dto/Suffix.kt
@@ -0,0 +1,8 @@
+package org.goafabric.calleeservice.controller.dto
+
+enum class Suffix(val display: String) {
+    BAR("Bar"),
+    BARON("Baron"),
+    BARONESS("Baroness"),
+    BARRONESSE("Baronesse"),
+}
