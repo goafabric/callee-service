e8c814907c3d46f77afa713ed6adfdb49cd4e891
Andreas Mautsch <amautsch>
2025-01-21 11:22:26 +0100

    spring doc annoations added

--- src/main/java/org/goafabric/calleeservice/Application.java
+++ src/main/java/org/goafabric/calleeservice/Application.java
@@ -1,5 +1,7 @@
 package org.goafabric.calleeservice;
 
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.annotation.RegisterReflection;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RegisterReflection(classes = {io.swagger.v3.core.jackson.mixin.Schema31Mixin.TypeSerializer.class, io.swagger.v3.oas.models.media.JsonSchema.class}, memberCategories = MemberCategory.INVOKE_DECLARED_CONSTRUCTORS)
 public class Application {
 
     public static void main(String[] args){
