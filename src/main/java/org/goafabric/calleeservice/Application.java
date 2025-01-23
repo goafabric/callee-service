3e0b36d2f112b82748d2a0785f6da05ac291ac0e
Andreas Mautsch <amautsch>
2025-01-23 08:26:36 +0100

    openapi test added

--- src/main/java/org/goafabric/calleeservice/Application.java
+++ src/main/java/org/goafabric/calleeservice/Application.java
@@ -5,8 +5,11 @@ import org.springframework.aot.hint.annotation.RegisterReflection;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 @RegisterReflection(classes = {io.swagger.v3.core.jackson.mixin.Schema31Mixin.TypeSerializer.class, io.swagger.v3.oas.models.media.JsonSchema.class, com.fasterxml.jackson.databind.BeanDescription.class}, memberCategories = {MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_METHODS})
@@ -17,8 +20,15 @@ public class Application {
     }
 
     @Bean
-    public CommandLineRunner init(ApplicationContext context) {
-        return args -> {if ((args.length > 0) && ("-check-integrity".equals(args[0]))) {SpringApplication.exit(context, () -> 0);}};
+    public CommandLineRunner init(ConfigurableApplicationContext context) {
+        return args -> {
+            if ((args.length > 0) && ("-check-integrity".equals(args[0]))) {
+                context.addApplicationListener((ApplicationListener<ApplicationReadyEvent>) event -> {
+                    RestClient.create().get().uri("http://localhost:" + context.getEnvironment().getProperty("local.server.port") + "/v3/api-docs").retrieve().body(String.class);
+                    SpringApplication.exit(context, () -> 0);
+                });
+            }
+        };
     }
 
 }
