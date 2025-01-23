e311243d8271e4bf250a47b009807f43cca33d7d
Andreas Mautsch <amautsch>
2025-01-23 08:31:27 +0100

    openapi test added

--- src/main/kotlin/org/goafabric/calleeservice/Application.kt
+++ src/main/kotlin/org/goafabric/calleeservice/Application.kt
@@ -1,5 +1,11 @@
 package org.goafabric.calleeservice
 
+import com.fasterxml.jackson.databind.BeanDescription
+import io.swagger.v3.core.jackson.mixin.Schema31Mixin
+import io.swagger.v3.oas.models.media.JsonSchema
+import org.springframework.aot.hint.MemberCategory
+import org.springframework.aot.hint.MemberCategory.INVOKE_PUBLIC_METHODS
+import org.springframework.aot.hint.annotation.RegisterReflection
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.ExitCodeGenerator
@@ -7,13 +13,15 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
-import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationListener
 import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestClient
 
 @SpringBootApplication
+@RegisterReflection(
+    classes = [Schema31Mixin.TypeSerializer::class, JsonSchema::class, BeanDescription::class],
+    memberCategories = [MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, INVOKE_PUBLIC_METHODS]
+)
 class Application(@Autowired private val context: ConfigurableApplicationContext):
     CommandLineRunner {
     override fun run(vararg args: String?) {
