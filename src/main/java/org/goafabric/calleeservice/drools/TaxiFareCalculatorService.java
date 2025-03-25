8855b92b32262f9adc1de6c487487aa3a3174b57
Andreas Mautsch <amautsch>
2025-03-25 07:56:35 +0100

    changes

--- src/main/java/org/goafabric/calleeservice/drools/TaxiFareCalculatorService.java
+++ src/main/java/org/goafabric/calleeservice/drools/TaxiFareCalculatorService.java
@@ -1,9 +1,10 @@
 package org.goafabric.calleeservice.drools;
 
+import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
+import org.kie.internal.io.ResourceFactory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportRuntimeHints;
@@ -12,16 +13,26 @@ import org.springframework.stereotype.Component;
 @Component
 @ImportRuntimeHints(TaxiFareCalculatorService.DroolsRuntimeHints.class)
 public class TaxiFareCalculatorService {
-    @Autowired
-    private KieContainer kieContainer;
 
     public Long calculateFare(TaxiRide taxiRide, Fare rideFare) {
-        var kieSession = kieContainer.newKieSession();
+        var kieSession = kieContainer().newKieSession();
         kieSession.setGlobal("rideFare", rideFare);
         kieSession.insert(taxiRide);
         kieSession.fireAllRules();
         kieSession.dispose();
         return rideFare.getRideFare();
+    }
+
+    @Bean
+    public KieContainer kieContainer() {
+        var kieServices = KieServices.Factory.get();
+        var kieFileSystem = kieServices.newKieFileSystem();
+        kieFileSystem.write(ResourceFactory.newClassPathResource("TAXI_FARE_RULE.drl"));
+        var kieBuilder = kieServices.newKieBuilder(kieFileSystem);
+
+        kieBuilder.buildAll();
+        var kieModule = kieBuilder.getKieModule();
+        return kieServices.newKieContainer(kieModule.getReleaseId());
     }
 
     static class DroolsRuntimeHints implements RuntimeHintsRegistrar {
@@ -32,12 +43,10 @@ public class TaxiFareCalculatorService {
     }
 
     @Bean
-    public CommandLineRunner init2(TaxiFareCalculatorService taxiFareCalculatorService) {
+    public CommandLineRunner droolsInit(TaxiFareCalculatorService taxiFareCalculatorService) {
         return args -> {
-            Long totalCharge = taxiFareCalculatorService.calculateFare(
-                    new TaxiRide(false, 5l), new Fare());
-
-            System.err.println(totalCharge);
+            System.err.println(taxiFareCalculatorService.calculateFare(
+                    new TaxiRide(false, 5l), new Fare()));
         };
     }
 
