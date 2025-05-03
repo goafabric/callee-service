76fbbe6189df46a120ce19245da3a8c9c2efdb5c
Andreas Mautsch <amautsch>
2025-05-03 14:53:41 +0200

    springdoc added

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.4.4"
 	id("io.spring.dependency-management") version "1.1.7"
-	//id("org.graalvm.buildtools.native") version "0.10.6"
+	id("org.graalvm.buildtools.native") version "0.10.6"
 
 	id("com.google.cloud.tools.jib") version "3.4.5"
 	id("net.researchgate.release") version "3.1.0"
@@ -93,4 +93,7 @@ configure<net.researchgate.release.ReleaseExtension> {
 openApi {
 	outputDir.set(file("doc/generated"))
 	customBootRun { args.set(listOf("--server.port=8080")) }
+	tasks.forkedSpringBootRun { dependsOn("compileAotJava", "processAotResources") }
 }
+tasks.cyclonedxBom { destination = file("doc/generated") }
+
