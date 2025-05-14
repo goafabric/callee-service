11b9f262b224a24a58d81c598b51e5b3e2d7fc7c
Andreas Mautsch <amautsch@gmx.de>
2025-05-14 12:46:17 +0000

    Update dependency net.ttddyy.observation:datasource-micrometer-spring-boot to v1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 		implementation("org.mapstruct:mapstruct:1.6.3")
 		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
 		implementation("io.github.resilience4j:resilience4j-spring-boot3:2.3.0")
-		implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:1.1.0")
+		implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:1.1.1")
 		testImplementation("com.tngtech.archunit:archunit-junit5:1.4.1")
 	}
 }
