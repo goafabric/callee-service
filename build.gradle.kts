a9fd0a9aeb2ace3419d5d51256795c46446b85f5
Andreas Mautsch <amautsch@gmx.de>
2025-05-23 01:51:25 +0000

    Update all-updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ val baseImage = "ibm-semeru-runtimes:open-21.0.3_9-jre-focal@sha256:5cb19afa9ee0
 plugins {
 	java
 	jacoco
-	id("org.springframework.boot") version "3.4.5"
+	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.graalvm.buildtools.native") version "0.10.6"
 
@@ -18,7 +18,7 @@ plugins {
 	id("net.researchgate.release") version "3.1.0"
 	id("org.sonarqube") version "6.2.0.5505"
 
-	id("org.cyclonedx.bom") version "2.3.0"
+	id("org.cyclonedx.bom") version "2.3.1"
 	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 
 	kotlin("jvm") version "2.1.21"
