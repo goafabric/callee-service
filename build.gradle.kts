65397a8e4f9ff0d08bcf7885c8c6cb7142332e63
Andreas Mautsch <amautsch>
2025-06-12 07:32:47 +0200

    native images

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,8 @@ val version: String by project
 java.sourceCompatibility = JavaVersion.VERSION_21
 
 val dockerRegistry = "goafabric"
-val baseImage = "ibm-semeru-runtimes:open-21.0.4.1_7-jre-focal@sha256:8b94f8b14fd1d4660f9dc777b1ad3630f847b8e3dc371203bcb857a5e74d6c39"
+//val baseImage = "ibm-semeru-runtimes:open-21.0.4.1_7-jre-focal@sha256:8b94f8b14fd1d4660f9dc777b1ad3630f847b8e3dc371203bcb857a5e74d6c39"
+val baseImage = "ibm-semeru-runtimes:open-21.0.4.1_7"
 
 plugins {
 	java
