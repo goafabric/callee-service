0e60973843c5a5e09a52d9711b06a3bbf2b61ce3
Andreas Mautsch <amautsch>
2025-07-15 15:27:28 +0200

    changes

--- src/main/java/org/goafabric/calleeservice/kubernetes/ProvisionLogic.java
+++ src/main/java/org/goafabric/calleeservice/kubernetes/ProvisionLogic.java
@@ -3,6 +3,7 @@ package org.goafabric.calleeservice.kubernetes;
 import io.fabric8.kubernetes.api.model.EnvVar;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodBuilder;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import org.slf4j.Logger;
@@ -21,7 +22,7 @@ import java.util.concurrent.TimeUnit;
 public class ProvisionLogic implements CommandLineRunner {
     private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
-    @Value("${application.images:goafabric/core-service-native:3.5.0,goafabric/person-service-native:3.5.0}")
+    @Value("${application.images:goafabric/person-service-native:3.5.1-SNAPSHOT,goafabric/core-service-native:3.5.0,}")
     private String applicationImages;
 
     @Autowired
@@ -36,6 +37,7 @@ public class ProvisionLogic implements CommandLineRunner {
 
     public void execute() {
         boolean async = false;
+        boolean update = true;
         String nameSpace = "example";
         List<String> tenantIds = List.of("0");
 
@@ -44,7 +46,9 @@ public class ProvisionLogic implements CommandLineRunner {
             Arrays.asList(applicationImages.split(",")).forEach(imageName ->
             {
                 try (KubernetesClient client = new KubernetesClientBuilder().build()) {
-                    createPod(client, false, nameSpace, imageName, tenantId);
+                    Integer replicaCount = scaleTo(client, "example", imageName, 0);
+                    createPod(client, nameSpace, imageName, tenantId);
+                    scaleTo(client, "example", imageName, replicaCount);
                 } catch (Exception e) {
                     log.error("error for tenant {} image {} cause {}", tenantId, imageName, e.getMessage());
                 }
@@ -54,9 +58,40 @@ public class ProvisionLogic implements CommandLineRunner {
 
     }
 
-    private void createPod(KubernetesClient client, boolean async, String nameSpace, String imageName, String tenantId) {
+    private Integer scaleTo(KubernetesClient client, String namespace, String imageName, Integer replicaCount) {
+        var deployment = findDeploymentByImage(client, namespace, imageName);
+
+        Integer replicas = deployment.getSpec().getReplicas();
+
+        log.info("scaling to {}" , replicaCount);
+        // Scale it to 0
+        client.apps()
+                .deployments()
+                .inNamespace(namespace)
+                .withName(deployment.getMetadata().getName())
+                .scale(replicaCount);
+        return replicas;
+    }
+
+
+    public static Deployment findDeploymentByImage(KubernetesClient client, String namespace, String imageName) {
+        return client.apps()
+                .deployments()
+                .inNamespace(namespace)
+                .list()
+                .getItems()
+                .stream()
+                .filter(deployment ->
+                        deployment.getSpec().getTemplate().getSpec().getContainers().stream()
+                                .anyMatch(container -> container.getImage().equals(imageName)))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No deployment found with image: " + imageName));
+    }
+
+    private void createPod(KubernetesClient client, String nameSpace, String imageName, String tenantId) {
         log.info("executing ... {}", imageName);
 
+        boolean async = false;
         String podName = imageName.split(":")[0].split("/")[1] + "-provision";
 
         // Delete existing Pod if it exists
