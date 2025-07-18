b6082b2059ec32b0b29ff6eb6e37e95f2f33b02e
Andreas Mautsch <amautsch>
2025-07-18 08:23:32 +0200

    upgrade

--- doc/examples/special/archunit_beanfactory.java
+++
@@ -1,8 +0,0 @@
-static class DoNotIncludeAOTClasses implements ImportOption {
-
-        @Override
-        public boolean includes(Location location) {
-            Pattern beanFactoryRegistrationsPatterns = Pattern.compile(".*_BeanFactoryRegistrations.*");
-            return !location.matches(beanFactoryRegistrationsPatterns);
-        }
-    }
