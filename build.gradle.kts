b762b0b641d7a1a317b48ffb408a7fc70f515a7f
Andreas Mautsch <amautsch@gmx.de>
2025-08-20 12:44:29 +0000

    Update dependency org.springdoc:springdoc-openapi-starter-webmvc-ui to v2.8.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
 	constraints {
 		annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 		implementation("org.mapstruct:mapstruct:1.6.3")
-		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.10")
 		implementation("io.github.resilience4j:resilience4j-spring-boot3:2.3.0")
 		implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:1.1.2")
 		testImplementation("com.tngtech.archunit:archunit-junit5:1.4.1")
