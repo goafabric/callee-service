3ac2af632de7e486899881d243258d7a6ec98a7b
Andreas Mautsch <amautsch>
2025-10-03 09:27:06 +0200

    upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,9 @@ val baseImage = "ibm-semeru-runtimes:open-21.0.4.1_7-jre-focal@sha256:8b94f8b14f
 plugins {
 	java
 	jacoco
-	id("org.springframework.boot") version "3.5.5"
+	id("org.springframework.boot") version "3.5.6"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("org.graalvm.buildtools.native") version "0.11.0"
+	id("org.graalvm.buildtools.native") version "0.11.1"
 
 	id("com.google.cloud.tools.jib") version "3.4.5"
 }
