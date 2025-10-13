7dc257c8c81f12f04e6b07f4528420644529ece6
Andreas Mautsch <amautsch>
2025-10-13 15:27:42 +0200

    ibm semeru

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ val javaVersion = "25"
 java.sourceCompatibility = JavaVersion.toVersion(javaVersion)
 
 val dockerRegistry = "goafabric"
-val baseImage = "eclipse-temurin:25-jre@sha256:74d5c631e5db5a44e7f5a2dd49f93f0c6f7b8c22c1dc1b8e1caec7009872c5c3"
+val baseImage = "ibm-semeru-runtimes:open-jdk-25.0.0_36-jre@sha256:8ae073345116cfd51ec37b26c3a1c25de9336d436354e0be4271bda1463e119c"
 
 plugins {
 	java
