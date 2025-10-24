9efeacac0ff92eecb10b23f65a7141274582fa0f
Andreas Mautsch <amautsch>
2025-10-24 09:09:59 +0200

    spring boot 4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ val baseImage = "ibm-semeru-runtimes:open-jdk-25.0.0_36-jre@sha256:8ae073345116c
 plugins {
 	java
 	jacoco
-	id("org.springframework.boot") version "4.0.0-M3"
+	id("org.springframework.boot") version "4.0.0-RC1"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.graalvm.buildtools.native") version "0.11.1"
 
