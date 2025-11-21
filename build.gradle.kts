a9c3d6defc800dc29a92705fcf2ce2115991d924
Andreas Mautsch <amautsch@gmx.de>
2025-11-21 01:45:43 +0000

    Update plugin org.springframework.boot to v4

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ val baseImage = "eclipse-temurin:25-jre@sha256:74d5c631e5db5a44e7f5a2dd49f93f0c6
 plugins {
 	java
 	jacoco
-	id("org.springframework.boot") version "3.5.7"
+	id("org.springframework.boot") version "4.0.0"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.graalvm.buildtools.native") version "0.11.3"
 
