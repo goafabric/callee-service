de5d0f36228cf4820d954e79febe98dca4ef8cd8
Andreas Mautsch <amautsch>
2025-11-22 08:12:35 +0100

    spring boot upgrade

--- src/main/java/org/goafabric/calleeservice/ApplicationRuntimeHints.java
+++ src/main/java/org/goafabric/calleeservice/ApplicationRuntimeHints.java
@@ -1,6 +1,7 @@
 package org.goafabric.calleeservice;
 
 import org.jspecify.annotations.Nullable;
+import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.TypeReference;
@@ -21,11 +22,8 @@ public class ApplicationRuntimeHints implements RuntimeHintsRegistrar {
                 TypeReference.of("org.springdoc.core.providers.SpringWebProvider$$SpringCGLIB$$0"),
                 builder -> builder.withField("CGLIB$CALLBACK_FILTER"));
 
-        /*
         hints.reflection().registerType(
                 TypeReference.of("org.springdoc.core.providers.SpringWebProvider"),
                 builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
-
-         */
     }
 }
