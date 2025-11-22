5e2c30637153ddd50efe8373d00ae5865040327f
Andreas Mautsch <amautsch>
2025-11-22 10:38:45 +0100

    spring boot upgrade

--- src/main/java/org/goafabric/calleeservice/Application.java
+++ src/main/java/org/goafabric/calleeservice/Application.java
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestClient;
 @SpringBootApplication
 public class Application{
 
-    public static void main(String[] args){
+    static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
 
