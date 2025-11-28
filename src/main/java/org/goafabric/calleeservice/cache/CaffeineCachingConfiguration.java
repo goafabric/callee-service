660bc4e4d95150ac5a2c688bf832461915c204c1
Andreas Mautsch <andreas.mautsch@cgm.com>
2025-11-28 14:09:10 +0100

    upgrade

--- src/main/java/org/goafabric/calleeservice/cache/CaffeineCachingConfiguration.java
+++ src/main/java/org/goafabric/calleeservice/cache/CaffeineCachingConfiguration.java
@@ -51,7 +51,6 @@ public class CaffeineCachingConfiguration implements CachingConfigurer {
         @Override
         public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
             try { //caffeine hints
-            hints.reflection().registerTypeIfPresent(classLoader, "com.github.benmanes.caffeine.cache.SSMS", MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
             hints.reflection().registerTypeIfPresent(classLoader, "com.github.benmanes.caffeine.cache.PSAMS", MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
             hints.reflection().registerTypeIfPresent(classLoader, "com.github.benmanes.caffeine.cache.SSMSA", MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
             hints.reflection().registerType(TypeReference.of("com.github.benmanes.caffeine.cache.SSMSA"),
