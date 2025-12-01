c699bc9fdb365afd703276886e2f8e5e2d26d090
Andreas Mautsch <amautsch>
2025-12-01 07:56:48 +0100

    spring boot upgrade

--- src/main/kotlin/org/goafabric/calleeservice/Application.kt
+++ src/main/kotlin/org/goafabric/calleeservice/Application.kt
@@ -1,15 +1,7 @@
 package org.goafabric.calleeservice
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.CommandLineRunner
-import org.springframework.boot.ExitCodeGenerator
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
-import org.springframework.context.ApplicationListener
-import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.web.client.RestClient
 
 @SpringBootApplication
 class Application
