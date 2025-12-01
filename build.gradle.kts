818010624424cea5e144b3f590c5495d0a62d4ab
Andreas Mautsch <amautsch>
2025-12-01 08:57:01 +0100

    spring boot upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,6 +113,6 @@ openApi {
 	tasks.forkedSpringBootRun { dependsOn("compileAotJava", "processAotResources") }
 }
 
-sonar { properties { property("sonar.exclusions", "**/ApplicationBaseRuntimeHints.java") } }
+sonar { properties { property("sonar.exclusions", "**/ApplicationBaseRuntimeHints.*") } }
 
 kotlin.compilerOptions.freeCompilerArgs.add("-Xannotation-default-target=param-property")
