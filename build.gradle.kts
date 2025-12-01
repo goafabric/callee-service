dd174e3bbc71550c0a5c2add85f6df82a9bafdc1
Andreas Mautsch <amautsch>
2025-12-01 08:57:21 +0100

    spring boot upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -97,4 +97,5 @@ openApi {
 	tasks.forkedSpringBootRun { dependsOn("compileAotJava", "processAotResources") }
 }
 
-sonar { properties { property("sonar.coverage.exclusions", "**/ApplicationBaseRuntimeHints.java"); property("sonar.cpd.exclusions", "**/ApplicationBaseRuntimeHints.java") } }
+
+sonar { properties { property("sonar.exclusions", "**/ApplicationBaseRuntimeHints.*") } }
