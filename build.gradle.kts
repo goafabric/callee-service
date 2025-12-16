6e4b8fe1f766c26c3c2819ec925503bdc0c21bf9
Andreas Mautsch <amautsch@gmx.de>
2025-12-16 12:59:50 +0000

    Update all-updates to v2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ plugins {
 	id("org.cyclonedx.bom") version "3.1.0"
 	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 
-	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
+	kotlin("jvm") version "2.3.0"
+	kotlin("plugin.spring") version "2.3.0"
 }
 
 repositories {
