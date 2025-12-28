66c5f1511c371c9c85b18bae1252a6285bea5ea2
Andreas Mautsch <amautsch>
2025-12-28 10:05:57 +0100

    spring boot upgrade

--- src/main/kotlin/org/goafabric/calleeservice/extensions/HttpInterceptor.kt
+++ src/main/kotlin/org/goafabric/calleeservice/extensions/HttpInterceptor.kt
@@ -1,6 +1,7 @@
 package org.goafabric.calleeservice.extensions
 
-import io.micrometer.common.KeyValue
+import io.opentelemetry.api.trace.Span
+import io.opentelemetry.context.Context
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.Logger
@@ -8,9 +9,7 @@ import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.server.observation.ServerRequestObservationContext
 import org.springframework.stereotype.Component
-import org.springframework.web.filter.ServerHttpObservationFilter
 import org.springframework.web.method.HandlerMethod
 import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.config.annotation.CorsRegistry
@@ -20,7 +19,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Component
 class HttpInterceptor : HandlerInterceptor {
     private val log: Logger = LoggerFactory.getLogger(this.javaClass.name)
-
+    
     @Configuration
     internal class Configurer : WebMvcConfigurer {
         @Value("\${cors.enabled:false}")
@@ -39,7 +38,7 @@ class HttpInterceptor : HandlerInterceptor {
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         UserContext.setContext(request)
-        configureLogsAndTracing(request)
+        configureLogsAndTracing()
 
         if (handler is HandlerMethod) {
             log.info(" {} method called for user {} ", handler.shortLogMessage, UserContext.userName)
@@ -48,18 +47,17 @@ class HttpInterceptor : HandlerInterceptor {
     }
 
     override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
-        UserContext.removeContext()
-        MDC.remove("tenantId")
+       afterCompletion()
     }
 
-    private fun configureLogsAndTracing(request: HttpServletRequest) {
+    private fun configureLogsAndTracing() {
+        Span.fromContext(Context.current()).setAttribute("tenant.id", UserContext.tenantId)
         MDC.put("tenantId", UserContext.tenantId)
-        ServerHttpObservationFilter.findObservationContext(request)
-            .ifPresent { context: ServerRequestObservationContext ->
-                context.addHighCardinalityKeyValue(
-                    KeyValue.of("tenant.id", UserContext.tenantId)
-                )
-            }
+    }
+
+    private fun afterCompletion() {
+        UserContext.removeContext()
+        MDC.remove("tenantId")
     }
 
 }
