543d8e640b3b0c2c94c84d1fb297c45f0f95fbef
Andreas Mautsch <amautsch@gmx.de>
2026-02-06 02:43:16 +0000

    Update plugin com.google.cloud.tools.jib to v3.5.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.graalvm.buildtools.native") version "0.11.4"
 
-	id("com.google.cloud.tools.jib") version "3.5.2"
+	id("com.google.cloud.tools.jib") version "3.5.3"
 	id("net.researchgate.release") version "3.1.0"
 	id("org.sonarqube") version "7.2.2.6593"
 
