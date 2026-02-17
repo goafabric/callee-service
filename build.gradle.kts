981b1766c92bd0bbe6ae86bb1bdfef684f9b5f9a
Andreas Mautsch <amautsch>
2026-02-17 08:19:45 +0100

    spring boot upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,6 @@ plugins {
 	id("net.researchgate.release") version "3.1.0"
 	id("org.sonarqube") version "7.2.2.6593"
 
-	id("org.cyclonedx.bom") version "3.1.0"
 	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 }
 
