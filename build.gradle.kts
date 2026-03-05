2b383501b8b683b96d454d2254b464afd40ecb0c
Andreas Mautsch <amautsch@gmx.de>
2026-03-05 13:17:22 +0000

    Update all-updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,11 +15,11 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "4.0.3"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("org.graalvm.buildtools.native") version "0.11.4"
+	id("org.graalvm.buildtools.native") version "0.11.5"
 
 	id("com.google.cloud.tools.jib") version "3.5.3"
 	id("net.researchgate.release") version "3.1.0"
-	id("org.sonarqube") version "7.2.2.6593"
+	id("org.sonarqube") version "7.2.3.7755"
 
 	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 
