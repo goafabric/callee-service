be2c5a6f054a33a348a625ddb51dfea204c63d47
Andreas Mautsch <amautsch>
2026-03-16 10:48:35 +0100

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
 		annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 		implementation("org.mapstruct:mapstruct:1.6.3")
 		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
-		implementation("io.github.resilience4j:resilience4j-spring-boot3:2.3.0")
+		implementation("io.github.resilience4j:resilience4j-spring-boot4:2.4.0")
 		implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:2.1.1")
 		implementation("org.mockito.kotlin:mockito-kotlin:6.2.3")
 		testImplementation("com.tngtech.archunit:archunit-junit5:1.4.1")
@@ -70,7 +70,7 @@ dependencies {
 	//test
 	testImplementation("org.mockito.kotlin:mockito-kotlin")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.github.resilience4j:resilience4j-spring-boot3")
+	testImplementation("io.github.resilience4j:resilience4j-spring-boot4")
 }
 
 tasks.withType<Test> {
