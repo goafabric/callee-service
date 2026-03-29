bde760113c5790f980cc99d72c69cdc6a824c2a9
Andreas Mautsch <amautsch>
2026-03-29 09:31:18 +0200

    fixes

--- src/main/kotlin/org/goafabric/calleeservice/ApplicationBaseRuntimeHints.kt
+++ src/main/kotlin/org/goafabric/calleeservice/ApplicationBaseRuntimeHints.kt
@@ -79,5 +79,10 @@ class ApplicationBaseRuntimeHints : RuntimeHintsRegistrar {
         hints.resources().registerPattern("db/migration/h2/*.sql")
         hints.resources().registerPattern("db/migration/common/*.sql")
         hints.resources().registerPattern("db/migration/postgresql/*.sql")
+
+        //org.graalvm.buildtools.native 1.0 regression
+        hints.reflection().registerType(
+            TypeReference.of("org.hibernate.validator.internal.util.logging.Log_\$logger"),
+            Consumer { builder: TypeHint.Builder? -> builder!!.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS) })
     }
 }
