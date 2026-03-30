3df051ac86f437b7994661a7faa0eef009a3ba70
Andreas Mautsch <amautsch>
2026-03-30 11:06:43 +0200

    fixes

--- src/main/java/org/goafabric/calleeservice/ApplicationBaseRuntimeHints.java
+++ src/main/java/org/goafabric/calleeservice/ApplicationBaseRuntimeHints.java
@@ -71,11 +71,14 @@ public class ApplicationBaseRuntimeHints implements RuntimeHintsRegistrar {
         hints.resources().registerPattern("db/migration/postgresql/*.sql");
 
         //org.graalvm.buildtools.native 1.0 regression
+        /*
         hints.reflection().registerType(TypeReference.of("org.hibernate.validator.internal.util.logging.Log_$logger"),
                 builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
 
         hints.reflection().registerType(TypeReference.of("org.hibernate.validator.internal.util.logging.Messages_$bundle"),
                 builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS).withField("INSTANCE"));
+
+         */
 
     }
 }
