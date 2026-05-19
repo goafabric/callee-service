7c9277f63838ecac819a9acbc8cb7e620c8e6ae5
Andreas Mautsch <amautsch>
2026-05-19 13:40:48 +0200

    chang

--- src/main/kotlin/org/goafabric/calleeservice/ApplicationBaseRuntimeHints.kt
+++ src/main/kotlin/org/goafabric/calleeservice/ApplicationBaseRuntimeHints.kt
@@ -93,6 +93,5 @@ class ApplicationBaseRuntimeHints : RuntimeHintsRegistrar {
         hints.reflection().registerType(
             TypeReference.of("org.springaicommunity.mcp.context.DefaultMetaProvider"),
             Consumer { builder: TypeHint.Builder? -> builder!!.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS) })
-
     }
 }
