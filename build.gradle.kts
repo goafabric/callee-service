a31eaf8f5904de5cefc273385ad1460780f93ae8
Andreas Mautsch <amautsch@gmx.de>
2026-07-30 14:20:55 +0000

    Update all-updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,14 +17,14 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.graalvm.buildtools.native") version "0.11.5"
 
-	id("com.google.cloud.tools.jib") version "3.5.3"
+	id("com.google.cloud.tools.jib") version "3.5.4"
 	id("net.researchgate.release") version "3.1.0"
-	id("org.sonarqube") version "7.2.3.7755"
+	id("org.sonarqube") version "7.3.1.8318"
 
 	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 
-	kotlin("jvm") version "2.3.20"
-	kotlin("plugin.spring") version "2.3.20"
+	kotlin("jvm") version "2.4.10"
+	kotlin("plugin.spring") version "2.4.10"
 }
 
 repositories {
@@ -37,11 +37,11 @@ dependencies {
 	constraints {
 		annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 		implementation("org.mapstruct:mapstruct:1.6.3")
-		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
 		implementation("io.github.resilience4j:resilience4j-spring-boot4:2.4.0")
-		implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:2.2.0")
+		implementation("net.ttddyy.observation:datasource-micrometer-spring-boot:2.2.1")
 		implementation("org.mockito.kotlin:mockito-kotlin:6.3.0")
-		testImplementation("com.tngtech.archunit:archunit-junit5:1.4.1")
+		testImplementation("com.tngtech.archunit:archunit-junit5:1.4.2")
 	}
 }
 
