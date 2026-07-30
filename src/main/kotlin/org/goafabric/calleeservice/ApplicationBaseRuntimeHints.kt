e5950ee666ce9ee6d2e104dbf35e04d4242e78bc
Andreas Mautsch <amautsch>
2026-07-30 08:40:46 +0200

    upgrade

--- src/main/kotlin/org/goafabric/calleeservice/ApplicationBaseRuntimeHints.kt
+++ src/main/kotlin/org/goafabric/calleeservice/ApplicationBaseRuntimeHints.kt
@@ -91,6 +91,10 @@ class ApplicationBaseRuntimeHints : RuntimeHintsRegistrar {
 
         //org.graalvm.buildtools.native 1.0 regression
         hints.reflection().registerType(
+            TypeReference.of("org.hibernate.validator.internal.util.logging.Log_\$logger"),
+            Consumer { builder: TypeHint.Builder? -> builder!!.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS) })
+
+        hints.reflection().registerType(
             TypeReference.of("com.google.protobuf.ExtensionRegistry"),
             Consumer { builder: TypeHint.Builder? -> builder!!.withMembers(MemberCategory.INVOKE_DECLARED_METHODS) })
     }
