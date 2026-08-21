39875e3fb293414566f3efbdd7518565601941e0
Andreas Mautsch <amautsch@gmx.de>
2026-08-21 01:44:37 +0000

    Update plugin org.springframework.boot to v4.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ val baseImage = "ibm-semeru-runtimes:open-jdk-25.0.2_10-jre-noble@sha256:b02e4cd
 plugins {
 	java
 	jacoco
-	id("org.springframework.boot") version "4.1.0"
+	id("org.springframework.boot") version "4.1.1"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.graalvm.buildtools.native") version "0.11.5"
 
